6941e24b0254f6e3d81013fdea6fae62094e65d5
Ryan Phillips <ryan.phillips@rackspace.com>
2015-01-09 14:22:29 -0600

    v0.0.2

---
+++ package.lua
@@ -0,0 +1,8 @@
+return {
+  name = "rphillips/luvit-async",
+  version = "0.0.2",
+  description = "async library for luvit",
+  author = "Ryan Phillips <ryan.phillips@rackspace.com>",
+  dependencies = {
+  },
+}
