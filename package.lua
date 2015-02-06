ae611fa9a85291303f94e750e406923b3e620dc4
Ryan Phillips <ryan.phillips@rackspace.com>
2015-02-06 12:53:50 -0600

    v0.3.2

--- package.lua
+++ package.lua
@@ -1,10 +1,8 @@
 return {
   name = "rphillips/line-emitter",
-  version = "0.3.1",
+  version = "0.3.2",
   description = "Library for buffering chunks of data and emitting lines",
   author = "Tomaz Muraus <tomaz@tomaz.me>",
   contributors = { "Ryan Phillips <ryan@trolocsis.com>" },
-  dependencies = {
-    ["bourbon"] = "https://github.com/racker/lua-bourbon/zipball/master",
-  }
+  dependencies = { }
 }
