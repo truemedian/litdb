e8dc70b546aba9b6be50fb471d07e7656a04e13c
Ryan Phillips <ryan.phillips@rackspace.com>
2015-02-19 20:43:04 -0600

    v0.3.0

---
+++ package.lua
@@ -0,0 +1,10 @@
+return {
+	name = "rphillips/options",
+	version = "0.3",
+	description = "easy to use getopt library",
+	author = "pancake <pancake@nopcode.org>",
+	dependencies = { },
+	files = {
+		"!test*"
+	}
+}
