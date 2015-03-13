adade2e4a9c5bda98c7abc39b4c2d67621130db7
Ryan Phillips <ryan.phillips@rackspace.com>
2015-03-13 17:08:23 -0500

    v0.2.2

--- package.lua
+++ package.lua
@@ -1,10 +1,10 @@
 return {
   name = "virgo-agent-toolkit/request",
-  version = "0.2.1",
+  version = "0.2.2",
   description = "Rackspace Monitoring Client",
   author = "Ryan Phillips <ryan.phillips@rackspace.com>",
   dependencies = {
-    "luvit/luvit@2.0.0",
+    "luvit/luvit@2.0.1",
     "rphillips/async@0.0.2",
   },
   files = {
