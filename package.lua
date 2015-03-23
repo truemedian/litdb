ac03c34039964dc2cc5c04fe81496a11ce40ee17
gsick <gamaliel.sick@yahoo.fr>
2015-03-23 09:06:14 +0100

    v0.0.2

---
+++ package.lua
@@ -0,0 +1,10 @@
+return {
+  name = "gsick/clocktime",
+  version = "0.0.2",
+  description = "C clocktime wrapped in lua ffi and packaged for lit",
+  author = "Gamaliel Sick",
+  homepage = "https://github.com/gsick/lit-clocktime",
+  files = {
+    "*.lua"
+  }
+}
