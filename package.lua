8ab3bb02bba8e141f2eba2133b70e7f91b590c70
gsick <gamaliel.sick@yahoo.fr>
2015-03-24 23:21:47 +0100

    v0.0.1

---
+++ package.lua
@@ -0,0 +1,13 @@
+return {
+  name = "gsick/crazy-snail",
+  version = "0.0.1",
+  description = "Real-Time Luvit module based on Redis",
+  author = "Gamaliel Sick",
+  homepage = "https://github.com/gsick/crazy-snail",
+  files = {
+    "*.lua",
+    "libs/$OS-$ARCH/*",
+    "!src",
+    "!tests"
+  }
+}
