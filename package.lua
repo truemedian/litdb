ae207649d5ba2742ae6f437d6d4d7649577b13eb
Ryan Phillips <ryan.phillips@rackspace.com>
2015-03-26 12:19:05 -0500

    v0.12.0

--- package.lua
+++ package.lua
@@ -1,13 +1,13 @@
 return {
   name = "virgo-agent-toolkit/virgo",
-  version = "0.11.5",
+  version = "0.12.0",
   dependencies = {
     "luvit/luvit@2.0.3",
     "rphillips/async@0.0.2",
     "rphillips/hsm@0.0.2",
-    "rphillips/line-emitter@0.3.3",
     "rphillips/logging@0.1.3",
-    "rphillips/split-stream@0.4.0",
+    "virgo-agent-toolkit/line-emitter@0.5.0",
+    "virgo-agent-toolkit/split-stream@0.5.3",
     "virgo-agent-toolkit/request@0.2.3",
   },
   files = {
