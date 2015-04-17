56ffb253a4c31cb691dd45a1fe810cc9fe29aa26
Tim Caswell <tim@creationix.com>
2015-04-17 16:59:11 -0500

    v0.1.3

---
+++ package.lua
@@ -0,0 +1,15 @@
+return {
+  name = "creationix/weblit",
+  version = "0.1.3",
+  dependencies = {
+    "creationix/weblit-logger@0.1.0",
+    "creationix/weblit-auto-headers@0.1.1",
+    "creationix/weblit-etag-cache@0.1.0",
+    "creationix/weblit-app@0.1.1",
+    "creationix/weblit-static@0.2.3",
+  },
+  files = {
+    "package.lua",
+    "init.lua",
+  }
+}
