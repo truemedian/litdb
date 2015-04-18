5aa33f6d5c913efe9cbc2bd88ed8f4ef953167c2
Ryan Phillips <ryan.phillips@rackspace.com>
2015-04-18 14:08:33 -0500

    v1.0.1

---
+++ package.lua
@@ -0,0 +1,14 @@
+return {
+  name = "virgo-agent-toolkit/async",
+  version = "1.0.1",
+  description = "async library for luvit",
+  author = "Ryan Phillips <ryan.phillips@rackspace.com>",
+  dependencies = {
+    "luvit/luvit@2"
+  },
+  files = {
+    "init.lua",
+    "queue.lua",
+    "package.lua"
+  }
+}
