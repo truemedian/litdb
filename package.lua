93b5cfb5cd90d4e693bb90176cba84ae516be015
Tim Caswell <tim@creationix.com>
2015-04-22 15:50:38 -0500

    v1.0.7

--- package.lua
+++ package.lua
@@ -1,9 +1,9 @@
 return {
   name = "creationix/hoedown",
-  version = "1.0.6",
+  version = "1.0.7",
   homepage = "https://github.com/creationix/lit-hoedown",
   dependencies = {
-    "creationix/ffi-loader@1.0.0"
+    "creationix/ffi-loader@1.0.2"
   },
   files = {
     "*.lua",
