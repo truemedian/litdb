7ebdfcbec88b368858b18cea8ae784984d2fdf15
Tim Caswell <tim@creationix.com>
2015-06-10 19:11:22 -0500

    v2.0.0

--- libs/vfs.lua
+++ libs/vfs.lua
@@ -1,3 +1,21 @@
+--[[
+
+Copyright 2014-2015 The Luvit Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS-IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+--]]
+
 local fs = require('coro-fs')
 local miniz = require('miniz')
 local pathJoin = require('luvi').path.join
@@ -55,7 +73,7 @@ local function zipFs(zip, autoChroot)
     local i = index + 1
     local num = zip:get_num_files()
     return function ()
-      while i < num do
+      while i <= num do
         local filename = zip:get_filename(i)
         i = i + 1
         if string.sub(filename, 1, #path) ~= path then return end
@@ -109,4 +127,3 @@ return function (path)
     return fs, path
   end
 end
-
