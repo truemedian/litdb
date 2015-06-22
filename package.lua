1581b84770913892b0d0a0818dfa87bf23f5bba6
gsick <gamaliel.sick@yahoo.fr>
2015-06-22 13:22:54 +0200

    v1.0.1

--- package.lua
+++ package.lua
@@ -1,6 +1,6 @@
 return {
   name = "gsick/uuid",
-  version = "1.0.0",
+  version = "1.0.1",
   description = "UUID generator",
   tags = {
     "uuid"
@@ -13,10 +13,6 @@ return {
     "Gamaliel Sick"
   },
   homepage = "https://github.com/gsick/lit-uuid",
-  dependencies = {
-    "gsick/clocktime@1.0.0",
-    "gsick/redis@0.0.1"
-  },
   files = {
     "*.lua",
     "!tests",
