553ea44aacce750b77ee81293d12d0b3183c1989
Ryan Phillips <ryan.phillips@rackspace.com>
2015-06-26 08:43:53 -0500

    v2.2.2

--- test.lua
+++ test.lua
@@ -1,14 +1,13 @@
-process.stdout:write('blah\n')
-process.stdout:write('blah\n')
-process.stdout:write('blah\n')
-process.stdout:write('blah\n')
-process.stdout:write('blah\n')
-process.stdout:write('blah\n')
-process.stdout:write('blah\n')
-process.stdout:write('blah\n')
-process.stdout:write('blah\n')
-process.stdout:write('blah\n')
-process.stdout:write('blah\n')
-process.stdout:write('blah\n')
-process.stdout:write('blah\n')
-process:exit(0)
+local https = require('https')
+local fs = require('fs')
+
+local options = {
+  key = fs.readFileSync('server.key.insecure'),
+  cert = fs.readFileSync('server.crt')
+}
+local server = https.createServer(options, function(req, res)
+  res:write('hello world\n')
+  res:finish()
+end)
+server:listen(8080, '127.0.0.1')
+
