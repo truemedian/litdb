503507d129a413342709ea85ea31d6090ca4d11d
Ryan Phillips <ryan.phillips@rackspace.com>
2015-07-09 08:39:59 -0500

    v2.2.7

--- test3.lua
+++ test3.lua
@@ -2,7 +2,7 @@ local http = require('http')
 
 http.createServer(function (req, res)
   res:writeHead(200, {['Content-Type'] = 'text/plain'})
-  res:finish('Hello World\n')
+  --res:finish('Hello World\n')
 end):listen(1337, '127.0.0.1')
 
 print('Server running at http://127.0.0.1:1337/')
