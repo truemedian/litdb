00766c1308ddc171bee904524e59a04020856dd4
Ryan Phillips <ryan.phillips@rackspace.com>
2015-08-05 01:09:14 -0500

    v0.6.1

--- package.lua
+++ package.lua
@@ -1,11 +1,11 @@
 return {
   name = "virgo-agent-toolkit/line-emitter",
-  version = "0.6.0",
+  version = "0.6.1",
   description = "Library for buffering chunks of data and emitting lines",
   author = "Tomaz Muraus <tomaz@tomaz.me>",
   contributors = { "Ryan Phillips <ryan@trolocsis.com>" },
   dependencies = {
-    "luvit/luvit@2.1.0"
+    "luvit/luvit@2"
   },
   files = {
     "**.lua",
