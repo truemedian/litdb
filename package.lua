f470dbcfdc27611e7dcacab7e65f89bc49004e20
james2doyle <james2doyle@gmail.com>
2015-09-05 13:16:06 -0700

    v0.0.2

--- package.lua
+++ package.lua
@@ -1,5 +1,22 @@
 return {
   name = "james2doyle/password-test",
-  version = "0.0.1",
-  homepage = "https://github.com/james2doyle/lit-password-test"
+  version = "0.0.2",
+  homepage = "https://github.com/james2doyle/lit-password-test",
+  description = "Test a password for strength, length, and custom rules",
+  tags = {
+    "password",
+    "test",
+    "strength",
+    "validate"
+  },
+  license = "MIT",
+  author = {
+    name = "James Doyle",
+    email = "james2doyle@gmail.com"
+  },
+  files = {
+    "*.lua",
+    "!tests",
+    "!examples"
+  }
 }
