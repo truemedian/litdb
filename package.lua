461c3ca13a825ed1228a6203654a68e9d20bc254
james2doyle <james2doyle@gmail.com>
2015-09-06 17:17:32 -0700

    v0.0.1

---
+++ package.lua
@@ -0,0 +1,22 @@
+return {
+  name = "james2doyle/fuzzy",
+  version = "0.0.1",
+  homepage = "https://github.com/james2doyle/lit-fuzzy",
+  description = "Preform a fuzzy comparison on 2 strings using the Levenshtein Distance algorithm",
+  tags = {
+    "fuzzy",
+    "search",
+    "levenshtein"
+  },
+  license = "MIT",
+  author = {
+    name = "James Doyle",
+    email = "james2doyle@gmail.com"
+  },
+  files = {
+    "*.lua",
+    "!fuzzy-test.lua",
+    "!tests",
+    "!examples"
+  }
+}
