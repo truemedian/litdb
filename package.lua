726bdab9c78ca406a025c7626c89eb9c166053b7
james2doyle <james2doyle@gmail.com>
2015-09-06 19:46:39 -0700

    v0.0.1

---
+++ package.lua
@@ -0,0 +1,22 @@
+return {
+  name = "james2doyle/valid-email",
+  version = "0.0.1",
+  homepage = "https://github.com/james2doyle/lit-valid-email",
+  description = "Check if an email is valid",
+  tags = {
+    "valid",
+    "email",
+    "address"
+  },
+  license = "MIT",
+  author = {
+    name = "James Doyle",
+    email = "james2doyle@gmail.com"
+  },
+  files = {
+    "*.lua",
+    "!email-test.lua",
+    "!tests",
+    "!examples"
+  }
+}
