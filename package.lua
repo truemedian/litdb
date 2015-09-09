13f01ad09d7a729c1b0c1002149e5314d808caad
kaustav haldar <hi@kaustav.me>
2015-09-09 16:33:22 -0700

    v1.0.2

--- package.lua
+++ package.lua
@@ -1,10 +1,15 @@
 return {
   name = 'kaustavha/luvit-walk',
-  version = "1.0.1",
+  version = "1.0.2",
   license = "Apache 2",
   homepage = "https://github.com/kaustavha/luvit-read-directory-recursive",
   description = "Recursively dives through directories and read the contents of all the children directories.",
   tags = {"luvit", "fs", "readdirrecursive" },
+  dependencies = { "luvit/luvit@2" },
   author = { name = 'Kaustav Haldar'},
-  main = { 'init.lua' }
+  main = { 'init.lua' },
+  files = {
+    '**.lua',
+    '!tests',
+  }
 }
