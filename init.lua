be9696dc18befa15806c9e48ca8dc80ecf05edd0
james2doyle <james2doyle@gmail.com>
2015-11-15 19:45:24 -0800

    v0.0.1

---
+++ init.lua
@@ -0,0 +1,37 @@
+-- converted from https://github.com/joaquimserafim/base64-url
+local Buffer = require('buffer').Buffer
+
+local base64 = {}
+
+local function unescape (str)
+  local len = 4 - #str % 4
+  local newstr = {}
+  str:gsub("%S+", function(c) table.insert(newstr, c) end)
+  for i = 1, len do
+    table.insert(newstr, "")
+  end
+  newstr = table.concat(newstr, '=')
+  newstr = newstr:gsub("(-)", "+")
+  newstr = newstr:gsub("(_)", "/")
+  return newstr
+end
+
+local function escape (str)
+  str = str:gsub("(%+)", "-")
+  str = str:gsub("(/)", "_")
+  str = str:gsub("(%=)", "")
+  return str
+end
+
+local function encode (str)
+  return escape(Buffer:new(str):toString())
+end
+
+local function decode (str)
+  return Buffer:new(unescape(str)):toString()
+end
+
+exports.unescape = unescape
+exports.escape = escape
+exports.encode = encode
+exports.decode = decode
---
+++ package.lua
@@ -0,0 +1,14 @@
+return {
+  name = "james2doyle/base64-url",
+  version = "0.0.1",
+  description = "Base64 encode, decode, escape and unescape for URL applications.",
+  tags = { "base64", "encode", "decode", "escape", "unescape", "url" },
+  license = "MIT",
+  author = { name = "james2doyle", email = "james2doyle@gmail.com" },
+  homepage = "https://github.com/james2doyle/lit-base64-url",
+  dependencies = {},
+  files = {
+    "**.lua",
+    "!test.lua"
+  }
+}
