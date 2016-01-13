a4ce2808525c230be6fec9f35f8765bd6d589e6e
Phil Hagelberg <phil@hagelb.org>
2016-01-13 21:35:28 +0700

    v0.3.2

---
+++ package.lua
@@ -0,0 +1,18 @@
+return {
+  name = "technomancy/irc",
+  version = "0.3.2",
+  description = "irc client module for luvit",
+  keywords = {"irc"},
+  homepage = "https://github.com/squeek502/luvit-irc",
+  author = {
+    name = "Ryan Liptak",
+    url = "http://www.ryanliptak.com"
+  },
+  dependencies = {
+    "luvit/luvit@2.1.1"
+  },
+  files = {
+    "!tests",
+    "**.lua"
+  }
+}
