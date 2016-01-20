db457bdc7ab3326903e939ccf24fc440dfda20a7
Ryan Phillips <ryan.phillips@rackspace.com>
2016-01-20 17:59:52 -0600

    v2.1.0

--- package.lua
+++ package.lua
@@ -1,8 +1,8 @@
 return {
   name = "virgo-agent-toolkit/virgo",
-  version = "2.0.12",
+  version = "2.1.0",
   luvi = {
-    version = "2.3.2-sigar",
+    version = "2.5.1-sigar",
     flavor = "sigar",
     url = "https://github.com/virgo-agent-toolkit/luvi/releases/download/v%s-sigar/luvi-%s-%s"
   },
