7b915984c1d250ad460956dd5ceba35107c09ae2
james2doyle <james2doyle@gmail.com>
2016-02-06 14:42:52 -0800

    v0.0.3

---
+++ package.lua
@@ -0,0 +1,20 @@
+return {
+  name = "james2doyle/string-trim",
+  version = "0.0.3",
+  homepage = "https://github.com/james2doyle/lit-string-trim",
+  description = "Trim the leading and trailing whitespace from a string",
+  tags = {
+    "trim",
+    "string",
+    "whitespace"
+  },
+  license = "MIT",
+  author = {
+    name = "James Doyle",
+    email = "james2doyle@gmail.com"
+  },
+  files = {
+    "*.lua",
+    "!test.lua"
+  }
+}
