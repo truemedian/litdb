41beefca367c47103f3f0d05d38d002ff2cce9cc
james2doyle <james2doyle@gmail.com>
2016-02-07 10:40:36 -0800

    v0.0.2

--- package.lua
+++ package.lua
@@ -1,8 +1,8 @@
 return {
   name = "james2doyle/merge",
-  version = "0.0.1",
-  description = "Merge table 1 with table 2",
-  tags = { "table", "merge", "combine", "array", "object" },
+  version = "0.0.2",
+  description = "Merge table 1 with table 2, handles nested tables",
+  tags = { "table", "merge", "combine", "nested", "array", "object" },
   license = "MIT",
   author = { name = "James Doyle", email = "james2doyle@gmail.com" },
   homepage = "https://github.com/james2doyle/lit-merge",
