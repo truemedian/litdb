1e6444bc6740925a28b941e7bdea021a0224f62b
Tim Caswell <tim@creationix.com>
2016-02-23 13:55:09 -0600

    v0.1.0

---
+++ package.lua
@@ -0,0 +1,16 @@
+return {
+  name = "creationix/wscat",
+  version = "0.1.0",
+  description = "A netcat like client for websockets",
+  luvi = {
+    version = "2.6.1",
+    flavor = "tiny",
+  },
+  homepage = "https://github.com/creationix/wscat",
+  dependencies = {
+    "creationix/websocket-client",
+    "luvit/readline",
+    "creationix/coro-split",
+    "luvit/pretty-print",
+  }
+}
