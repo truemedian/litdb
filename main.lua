45bade3f7d4a4da8b22b1978d30ca23fb07bb78a
Tim Caswell <tim@creationix.com>
2016-03-18 23:25:01 -0500

    v0.0.1

---
+++ main.lua
@@ -0,0 +1,43 @@
+local bundle = require('luvi').bundle
+loadstring(bundle.readfile("luvit-loader.lua"), "bundle:luvit-loader.lua")()
+local msgpackEncode = require('msgpack').encode
+local connect = require('coro-net').connect
+local pathJoin = require('pathjoin').pathJoin
+local getenv = require('os').getenv
+
+local cwd = require('uv').cwd()
+local function resolve(path)
+  return pathJoin(cwd, path)
+end
+
+
+local function parseArgs(command, ...)
+  if not command then
+    print "Usage:"
+    print "\trax command [args...]"
+    os.exit(-1)
+  end
+  if command == "serve" then
+    return require('daemon')
+  end
+  local args = table.pack(...)
+  for i = 1, args.n do
+    if args[i]:sub(1,1) ~= "-" then
+      args[i] = resolve(args[i])
+    end
+  end
+  coroutine.wrap(function ()
+    local key = getenv("RAX_CLIENT_KEY")
+    print("Sending '" .. command .. "' command to client")
+    local _, write = assert(connect({
+      host = "127.0.0.1",
+      port = 13377,
+    }))
+    write(msgpackEncode{key, command, unpack(args)})
+  end)()
+end
+
+parseArgs(...)
+
+
+require('uv').run()
---
+++ package.lua
@@ -0,0 +1,19 @@
+return {
+  name = "virgo-agent-toolkit/rax",
+  version = "0.0.1",
+  description = "A super agent. Daemon and CLI tool.",
+  luvi = {
+    version = "2.6.1",
+    flavor = "regular",
+  },
+  homepage = "https://github.com/virgo-agent-toolkit/super-agent",
+  dependencies = {
+    "creationix/websocket-client",
+    "luvit/pretty-print",
+    "creationix/msgpack",
+    "creationix/schema",
+    "creationix/coro-fs",
+    "luvit/json",
+    "luvit/secure-socket",
+  }
+}
