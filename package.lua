faf412511c72207bf1737ab859599a08cf49f48f
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2016-08-15 15:53:31 +0200

    v1.0.0

---
+++ package.lua
@@ -0,0 +1,17 @@
+-- for older versions of luvit and alternative package managers
+return {
+  name = "voronianski/utopia",
+  version = "1.0.0-beta",
+  description = "High performance middleware framework for Luvit.io",
+  repository = {
+    url = "http://github.com/luvitrocks/luvit-utopia.git",
+  },
+  tags = {"utopia", "express", "connect"},
+  author = {
+    name = "Dmitri Voronianski",
+    email = "dmitri.voronianski@gmail.com"
+  },
+  homepage = "https://github.com/luvitrocks/luvit-utopia",
+  licenses = {"MIT"},
+  main = 'init.lua'
+}
