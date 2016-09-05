6ee87a296b5d54aad9cbe73006507763d756d8b4
Darmie Akinlaja <dreakinlaja@gmail.com>
2016-09-05 11:22:10 +0100

    v0.0.1

---
+++ package.lua
@@ -0,0 +1,11 @@
+return {
+  name = "darmie/luvit-base64",
+  version = "0.0.1",
+  homepage = 'https://github.com/darmie/luvit-base64',
+  files = {
+    "*.lua",
+    "csrc/libb64/*.h",
+    "csrc/libb64/*.c",
+    "$OS-$ARCH/*",
+  }
+}
