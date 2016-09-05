002659ce7fae1c8e453343c89a3bcabcad5331f1
Darmie Akinlaja <dreakinlaja@gmail.com>
2016-09-05 08:27:27 +0100

    v0.0.3

--- package.lua
+++ package.lua
@@ -1,6 +1,7 @@
 return {
   name = "darmie/luvit-exceptions",
-  version = "0.0.2",
+  version = "0.0.3",
+  homepage = 'https://github.com/darmie/luvit-exceptions',
   files = {
     "*.lua",
     "src/*.lua",
