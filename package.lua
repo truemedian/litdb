9c5f438ff270c70e0d1b9239ec7709b3d1a7744c
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2016-09-25 21:39:21 +0200

    v1.0.0

---
+++ package.lua
@@ -0,0 +1,21 @@
+return {
+  name = 'voronianski/static',
+  version = '1.0.0',
+  description = 'Serve static files with luvit.io and Utopia',
+  repository = {
+    url = 'http://github.com/luvitrocks/luvit-static.git',
+  },
+  tags = {'utopia', 'express', 'connect', 'middleware', 'server', 'static', 'files'},
+  author = {
+    name = 'Dmitri Voronianski',
+    email = 'dmitri.voronianski@gmail.com'
+  },
+  homepage = 'https://github.com/luvitrocks/luvit-utopia',
+  licenses = {'MIT'},
+  dependencies = {},
+  files = {
+    '**.lua',
+    '!test*',
+    '!example*'
+  }
+}
