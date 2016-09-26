23bb620dbdd1d046367fb7d3fe9d6679cb10de75
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2016-09-26 22:33:34 +0200

    v1.0.0

---
+++ package.lua
@@ -0,0 +1,22 @@
+return {
+  name = 'voronianski/timeout',
+  version = '1.0.0',
+  description = 'Luvit.io server timeout middleware',
+  repository = {
+    url = 'http://github.com/luvitrocks/luvit-timeout.git',
+  },
+  tags = {'utopia', 'express', 'connect', 'middleware', 'server'},
+  author = {
+    name = 'Dmitri Voronianski',
+    email = 'dmitri.voronianski@gmail.com'
+  },
+  homepage = 'https://github.com/luvitrocks/luvit-timeout',
+  licenses = {'MIT'},
+  dependencies = {},
+  files = {
+    '**.lua',
+    '!test*',
+    '!example*'
+  }
+}
+
