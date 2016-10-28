998955a498d6a647b1011ea66cd92119f531b2b4
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2016-10-28 22:32:36 +0200

    v1.0.0

---
+++ package.lua
@@ -0,0 +1,23 @@
+return {
+  name = 'voronianski/ludash',
+  version = '1.0.0',
+  description = 'lλdash - Luvit.io functional programming helper library',
+  repository = {
+    url = 'http://github.com/luvitrocks/ludash.git',
+  },
+  tags = {'luvit', 'lodash', 'ludash', 'functional', 'fp', 'utility', 'map', 'reduce', 'each', 'functions', 'underscore'},
+  author = {
+    name = 'Dmitri Voronianski',
+    email = 'dmitri.voronianski@gmail.com'
+  },
+  homepage = 'https://github.com/luvitrocks/utopia',
+  licenses = {'MIT'},
+  dependencies = {
+    'filwisher/lua-tape'
+  },
+  files = {
+    '**.lua',
+    '!test*',
+    '!example*'
+  }
+}
