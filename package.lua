266d39aa75308121eb0d9a7ff4d20aad0d5d259c
pyros2097 <pyros2097@gmail.com>
2017-01-25 11:12:16 +0530

    v0.0.1

---
+++ package.lua
@@ -0,0 +1,15 @@
+  return {
+    name = "pyros2097/graphql",
+    version = "0.0.1",
+    description = "A simple description of my little package.",
+    tags = { "lua", "lit", "luvit" },
+    license = "MIT",
+    author = { name = "pyros2097", email = "pyros2097@gmail.com" },
+    homepage = "https://github.com/pyros2097/graphql",
+    dependencies = {},
+    files = {
+      "**.lua",
+      "!test*"
+    }
+  }
+  
