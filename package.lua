ffb77cf7162dd7f43ee6b39bc4c04892fe5227a0
DannehSC <dangerspookycanyon@gmail.com>
2017-03-03 17:55:45 -0600

    v0.0.1

---
+++ package.lua
@@ -0,0 +1,10 @@
+return {
+	name = 'DannehSC/luvit-firebase',
+	version = '0.0.1',
+	homepage = 'https://github.com/DannehSC/encrypter',
+	dependencies = {},
+	tags = {'luvit', 'api', 'encrypter'},
+	license = '',
+	author = 'DannehSC',
+	files = {'**.lua'},
+}
