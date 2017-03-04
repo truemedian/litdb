c5aace442d1a87edfcd4bac624fb30f8a2713843
DannehSC <dangerspookycanyon@gmail.com>
2017-03-03 18:06:21 -0600

    v0.0.3

--- package.lua
+++ package.lua
@@ -1,10 +1,10 @@
 return {
 	name = 'DannehSC/encrypter',
-	version = '0.0.2',
+	version = '0.0.3',
 	homepage = 'https://github.com/DannehSC/encrypter',
 	dependencies = {},
 	tags = {'luvit', 'api', 'encrypter'},
-	license = '',
+	license = 'none',
 	author = 'DannehSC',
 	files = {'**.lua'},
 }
