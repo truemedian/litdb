f59f1a81cdde8d4a19c4f58f0ffed9823e833981
ThrownException <Djmann1013@gmail.com>
2017-03-08 05:09:39 -0800

    v1.0.0

---
+++ package.lua
@@ -0,0 +1,14 @@
+  return {
+    name = "MrTeenageParker/sandboxxy",
+    version = "1.0.0",
+    description = "A luvit lua sandbox which allows for implementation with Discordia to allow sandboxed execution",
+    tags = { "lua", "discordia" },
+    license = "MIT",
+    author = { name = "MrTeenageParker", email = "parker@jacobparker.pw" },
+    homepage = "https://github.com/MrTeenageParker/sandboxxy",
+    dependencies = {},
+    files = {
+      "**.lua",
+      "!test*"
+    }
+  }
