1f7f4f0ee211c05113cc64dc8e9f5871f0627580
Tim Caswell <tim@creationix.com>
2017-06-14 12:09:02 -0500

    v3.0.1

--- file.lua
+++ file.lua
@@ -1,7 +1,7 @@
 --[[lit-meta
   name = "creationix/websocket-codec"
   description = "A codec implementing websocket framing and helpers for handshakeing"
-  version = "3.0.0"
+  version = "3.0.1"
   dependencies = {
     "creationix/base64@2.0.0",
     "creationix/sha1@1.0.0",
@@ -67,13 +67,13 @@ local function decode(chunk, index)
   local len = band(second, 0x7f)
   local offset
   if len == 126 then
-    if #chunk < 4 then return end
+    if length < 4 then return end
     len = bor(
       lshift(byte(chunk, start + 3), 8),
       byte(chunk, start + 4))
     offset = 4
   elseif len == 127 then
-    if #chunk < 10 then return end
+    if length < 10 then return end
     len = bor(
       lshift(byte(chunk, start + 3), 24),
       lshift(byte(chunk, start + 4), 16),
@@ -94,7 +94,7 @@ local function decode(chunk, index)
     offset = offset + 4
   end
   offset = offset + start
-  if #chunk < offset + len then return end
+  if length < offset + len then return end
 
   local first = byte(chunk, start + 1)
   local payload = sub(chunk, offset + 1, offset + len)
