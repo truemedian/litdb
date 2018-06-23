7465dbe54703a0356475f102f1f79e287dc69868
Oliver Hensworth <oliverhensworth1@gmail.com>
2018-06-23 14:53:12 +0100

    v2.0.6

--- containers/games.lua
+++ containers/games.lua
@@ -9,15 +9,11 @@ return function(id, callback)
     if type(id) ~= "number" then return error(id.." is not a number.") end
     if math.floor(id) ~= id then return error(id.." is not a full number. No decimals.") end
     local info = {}
+    
     coroutine.wrap(function()
-        local _,body = request("GET", format(endpoints.userId, id))
+        local _,body = request("GET", format(endpoints.gameInfo, id))
 
         local info = decode(body)
-    	info = {
-    		username = info.Username,
-    		userid = info.UserId,
-    		isOnline = info.IsOnline,
-    	}
 
         callback(info)
     end)()
