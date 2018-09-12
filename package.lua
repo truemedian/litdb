37e85520be038a2cbbc8693d01fa9e2fdcaea5a6
mrtnpwn <martin2004sexyhola@gmail.com>
2018-09-11 21:48:22 -0500

    v1.0.0

---
+++ package.lua
@@ -0,0 +1,14 @@
+  return {
+    name = "mrtnpwn/luv4",
+    version = "1.0.0",
+    description = "A UUID Version 4 package for Luvit, zero dependencies and 1Kb size.",
+    tags = { "lua", "lit", "luvit", "uuid" },
+    license = "MIT",
+    author = { name = "mrtnpwn" },
+    homepage = "https://github.com/mrtnpwn/luv4",
+    files = {
+      "**.lua",
+      "!test*"
+    }
+  }
+  
