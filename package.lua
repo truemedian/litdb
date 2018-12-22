401285b2b1dc31ec02e84f95cd4f2bd171980ec5
Lautenschlager-id <tai.romani@hotmail.com>
2018-12-22 03:14:55 -0200

    v0.1.0

---
+++ package.lua
@@ -0,0 +1,14 @@
+return {
+	name = "Lautenschlager-id/fromage",
+	version = "0.1.0",
+	homepage = "https://github.com/Lautenschlager-id/Fromage",
+	dependencies = {
+		"creationix/coro-http@3.0.0"
+	},
+	tags = { "atelier801", "forum", "transformice", "api" },
+	license = "MIT",
+	author = "Lautenschlager.id",
+	files = {
+		"*.lua"
+	}
+}
