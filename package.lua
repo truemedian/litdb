45e20d38a588974e5069b8eb7891dac3491b3115
Martin Aguilar <martin2004sexyhola@gmail.com>
2019-01-28 21:22:57 -0600

    v1.0.0

--- package.lua
+++ package.lua
@@ -1,6 +1,6 @@
 return {
   name = "mrtnpwn/plug",
-  version = "0.0.1",
+  version = "1.0.0",
   description = "The *awesome* Mini World: Block Art Plugin Framework.",
   tags = { "lua", "luvit", "mwba", "plugin", "framework" },
   license = "MIT",
