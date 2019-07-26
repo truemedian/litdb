680e4f75dc83662d39fa52cf05312aa436534a3f
Martín Aguilar <ik7swordking@gmail.com>
2019-07-26 10:43:20 -0500

    v0.1.1

---
+++ package.lua
@@ -0,0 +1,15 @@
+  return {
+    name = "mrtnpwn/typical",
+    version = "0.1.1",
+    description = "Fancy type names for Lua",
+    tags = { "lua", "metatables", "type" },
+    license = "MIT",
+    author = { name = "Rob Hoelz", email = "rob@hoelz.ro" },
+    homepage = "https://github.com/mrtnpwn/typical",
+    dependencies = {},
+    files = {
+      "**.lua",
+      "!test*"
+    }
+  }
+  
