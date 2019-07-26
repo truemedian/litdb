80534c97cb4fda816606a0c742f93c4ad87fa93f
Tim Caswell <tcaswell@magicleap.com>
2019-07-26 08:53:58 -0500

    v0.0.3

--- package.lua
+++ package.lua
@@ -1,12 +1,16 @@
 return {
     name = "creationix/mpk-serve",
-    version = "0.0.2",
+    version = "0.0.3",
     luvi = {
         -- Configure this section for the kind of `lit make` you wish to have.
         -- This first option embeds tiny luvi inside the binary
         flavor = "tiny"
         -- This second option embeds a shebang line pointing to the system luvi
         -- inline = "#!/usr/local/bin/luvi --\n"
+    },
+    files = {
+        "*.lua",
+        "libs/*.lua"
     },
     dependencies = {
         "luvit/pretty-print",
