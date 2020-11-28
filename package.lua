d21ec2f0a2113584c2419a3bc5ca09640d7f757f
Nameless <truemedian@gmail.com>
2020-11-27 21:10:38 -0600

    v0.1.0

---
+++ package.lua
@@ -0,0 +1,13 @@
+return {
+    name = 'truemedian/rethink-luvit',
+    version = '0.1.0',
+    dependencies = {
+        'creationix/coro-net@3.2.0',
+        'luvit/secure-socket@1.2.2',
+    },
+    license = 'MIT',
+    homepage = 'https://github.com/truemedian/rethink-luvit',
+    description = 'A rethinkdb driver for Luvit.',
+    tags = {'luvit', 'rethinkdb', 'database', 'driver'},
+    author = {name = 'TrueMedian'},
+}
