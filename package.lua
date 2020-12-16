c4dbd59697877dadc4c52af2b3a7ef6c425a2173
Soviet Kitsune <sovietkitsune@soviet.solutions>
2020-12-16 13:52:09 -0800

    v0.3.8

--- package.lua
+++ package.lua
@@ -1,14 +1,13 @@
 return {
    name = 'SovietKitsune/SuperToast',
-   version = '0.3.7',
+   version = '0.3.8',
    description = 'The better Toast',
    tags = {'toast', 'discordia'},
    license = 'MIT',
    author = {name = 'Soviet Kitsune', email = 'sovietkitsune@soviet.solutions'},
    homepage = 'https://github.com/SovietKitsune/SuperToast',
    dependencies = {
-      'SinisterRectus/discordia@2.8.4',
-      'SovietKitsune/typed@1.0.2'
+      'SinisterRectus/discordia@2.8.4'
    },
    files = {
       '**.lua',
