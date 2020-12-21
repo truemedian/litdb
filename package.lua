43c706a924bf7d3f03c349e5f0d9fc314d989c6b
Lautenschlager <tai.romani@hotmail.com>
2020-12-21 13:58:05 -0300

    v0.1.4

---
+++ package.lua
@@ -0,0 +1,18 @@
+return {
+	version = "0.1.4",
+
+	name = "Lautenschlager-id/prepdir",
+	author = "Lautenschlager-id",
+
+	homepage = "https://github.com/Lautenschlager-id/prepdir",
+	license = "MIT",
+
+	files = { "*.lua" },
+	dependencies = { },
+	tags = {
+		"lua",
+		"luvit",
+		"preprocessing",
+		"directive"
+	}
+}
