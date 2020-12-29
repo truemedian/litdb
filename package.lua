61cb6e6b751819cc593045e0bd33f7212bbb2876
canada <NotSoClassy357@gmail.com>
2020-12-29 11:36:38 -0500

    v1.3.0

--- package.lua
+++ package.lua
@@ -1,6 +1,6 @@
   return {
     name = "NotSoClassy/toast",
-    version = "1.2.9",
+    version = "1.3.0",
     description = "A Framework for Discordia",
     tags = { "discordia", "discord" },
     license = "MIT",
