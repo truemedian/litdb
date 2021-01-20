05da76ecb34ba4e030815464374cc47c7a6ed391
Git (lethel) <minebuckbr@gmail.com>
2021-01-20 07:50:25 -0300

    v1.0.2

---
+++ file.lua
@@ -0,0 +1,16 @@
+  return {
+    name = "letiulthecode/Old-Machine",
+    version = "1.0.2",
+    description = "A mega simple bot made in one file and discordia without any external libraries",
+    tags = { "discordia", "luvit" },
+    license = "MIT",
+    author = { name = "Git (lethel)", email = "minebuckbr@gmail.com" },
+    homepage = "https://github.com/letiulthecode/Old-Machine",
+    dependencies = {
+      'discordia'
+    },
+    files = {
+      "bot.lua",
+      "test/*.lua"
+    }
+  }
