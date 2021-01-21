2177234bc6cfb45548589ec4d64df0afc3b88f5b
Git (lethel) <minebuckbr@gmail.com>
2021-01-21 13:04:11 -0300

    v1.0.0

---
+++ file.lua
@@ -0,0 +1,17 @@
+  return {
+    name = "letiulthecode/Bread",
+    version = "1.0.0",
+    description = "A command handler for discordia",
+    tags = { "lua", "luvit", "discordia" },
+    license = "MIT",
+    author = { name = "Git (lethel)", email = "minebuckbr@gmail.com" },
+    homepage = "https://github.com/letiulthecode/Bread",
+    dependencies = {
+      "SinisterRectus/discordia"
+    },
+    files = {
+      "./Bread/*.lua",
+      "./Bread/examples/*.lua"
+    }
+  }
+  
