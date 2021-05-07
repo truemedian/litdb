d547c7f7edf2207a259eab0ef7d5caf1505407eb
Xinshou <ShahriaFahim@live.com>
2021-05-07 13:23:51 +0600

    v1.0.0

---
+++ init.lua
@@ -0,0 +1,12 @@
+  --[[lit-meta
+    name = "Discord OAuth2 "
+    version = "1.0.0"
+    dependencies = {}
+    description = "Create your own OAuth2 session for Discord"
+    tags = { "Luvit", "Weblit", "Lua" }
+    license = "MIT"
+    author = { name = "Xinshou", email = "ShahriaFahim@live.com" }
+    homepage = "https://github.com/leXinshou/DiscordOAuth2"
+  ]]
+  
+  require("./DiscordOAuth2")
---
+++ package.lua
@@ -0,0 +1,14 @@
+  return {
+    name = "LeXinshou/discordoauth2",
+    version = "1.0.0",
+    description = "Create your own OAuth2 session for Discord",
+    tags = { "Luvit", "Weblit", "Lua" },
+    license = "MIT",
+    author = { name = "LeXinshou", email = "ShahriaFahim@live.com" },
+    homepage = "https://github.com/leXinshou/DiscordOAuth2",
+    dependencies = {},
+    files = {
+      "**.lua"      
+    }
+  }
+  
