b96cdfc1ff135e7b30f55511c447950b7e641a99
AntwanR942 <antwang942@gmail.com>
2021-09-10 17:48:19 +0100

    v2.9.1

---
+++ package.lua
@@ -0,0 +1,15 @@
+  return {
+    name = "AntwanR942/discordia",
+    version = "2.9.1",
+    description = "Fork of SinisterRectus/Discordia",
+    tags = { "discord", "api" },
+    license = "MIT",
+    author = { name = "Anthony", email = "antwang942@gmail.com" },
+    homepage = "https://github.com/AntwanR942/Discordia",
+    dependencies = {},
+    files = {
+      "**.lua",
+      "!test*"
+    }
+  }
+  
