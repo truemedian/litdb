22914fec461c253faa1b79ba272020a7ac2a3f10
AntwanR942 <antwang942@gmail.com>
2021-09-10 18:12:32 +0100

    v2.9.2

--- package.lua
+++ package.lua
@@ -1,15 +1,34 @@
-  return {
-    name = "AntwanR942/discordia",
-    version = "2.9.1",
-    description = "Fork of SinisterRectus/Discordia",
-    tags = { "discord", "api" },
-    license = "MIT",
-    author = { name = "Anthony", email = "antwang942@gmail.com" },
-    homepage = "https://github.com/AntwanR942/Discordia",
-    dependencies = {},
-    files = {
-      "**.lua",
-      "!test*"
-    }
-  }
-  
+--[[The MIT License (MIT)
+Copyright (c) 2016-2020 SinisterRectus
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.]]
+
+return {
+	name = 'AntwanR942/discordia',
+	version = '2.9.2',
+	homepage = 'https://github.com/AntwanR942/Discordia',
+	dependencies = {
+		'creationix/coro-http@3.1.0',
+		'creationix/coro-websocket@3.1.0',
+    'creationix/coro-spawn@3.0.2',
+    'SinisterRectus/sqlite3@1.0.1',
+		'luvit/secure-socket@1.2.2',
+	},
+	tags = {'discord', 'api'},
+	license = 'MIT',
+	author = 'Sinister Rectus',
+	files = {'**.lua'},
+}
