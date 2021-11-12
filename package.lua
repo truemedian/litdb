872e3ef2643cbbdb669fdcdf9c3933a36dd9e255
Nameless <truemedian@gmail.com>
2021-11-11 18:06:30 -0600

    v0.2.0

--- package.lua
+++ package.lua
@@ -1,9 +1,9 @@
 return {
     name = 'truemedian/rethink-luvit',
-    version = '0.1.0',
+    version = '0.2.0',
     dependencies = {
-        'creationix/coro-net@3.2.0',
-        'luvit/secure-socket@1.2.2',
+        'creationix/coro-net@3.3.0',
+        'luvit/secure-socket@1.2.3',
     },
     license = 'MIT',
     homepage = 'https://github.com/truemedian/rethink-luvit',
