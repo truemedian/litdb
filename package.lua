273764c91261625356930328b1516f1f598abf99
Laminariy <dovakin121212@gmail.com>
2021-11-13 22:05:25 +0300

    v0.0.1

---
+++ package.lua
@@ -0,0 +1,16 @@
+  return {
+    name = "Laminariy/vk-luvit",
+    version = "0.0.1",
+    description = "VK API wrapper for luvit",
+    tags = { "coro", "http", "vk", "api", "wrapper" },
+    license = "MIT",
+    author = { name = "Laminariy", email = "dovakin121212@gmail.com" },
+    homepage = "https://github.com/Laminariy/vk-luvit",
+    dependencies = {
+      "RiskoZoSlovenska/simple-http"
+    },
+    files = {
+      "**.lua",
+      "!test*"
+    }
+  }
