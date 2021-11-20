8786ed9e2baa76843cc4529d8175935ca55fab13
RiskoZoSlovenska <riskozoslovenska+git@gmail.com>
2021-11-20 15:51:26 -0500

    v1.1.0

--- file.lua
+++ file.lua
@@ -1,6 +1,6 @@
 --[[lit-meta
 	name = "RiskoZoSlovenska/simple-http"
-	version = "1.0.0"
+	version = "1.1.0"
 	homepage = "https://github.com/RiskoZoSlovenska/simple-http"
 	description = "A basic, high-level wrapper for coro-http."
 	tags = {"http", "coro", "wrapper"}
@@ -15,21 +15,37 @@
 local http = require("coro-http")
 local json = require("json")
 local querystring = require("querystring")
-local schema = require("schema")
+local schemaLib = require("schema")
 
 local match, lower, format = string.match, string.lower, string.format
 local insert = table.insert
-local pairs = pairs
-local tostring = tostring
-local type = type
-local pcall = pcall
 
 local Encoding = {
 	json = "application/json",
 	url  = "application/x-www-form-urlencoded",
 }
 
+local FailReason = {
+	Unknown = 0,
+	BadPayload = 1,
+	RequestError = 2,
+	BadResponseCode = 3,
+	BadResponseBody = 4,
+	InvalidResponseBody = 5,
+}
 
+
+
+-- Assumes func never returns more than two values, and that a valid return won't ever be false or nil
+local function customPcall(func, ...)
+	local success, res1, res2 = xpcall(func, debug.traceback, ...)
+
+	if success then
+		return res1, res2
+	else
+		return nil, res1
+	end
+end
 
 local function matchContentType(typeString)
 	return match(typeString, "^([%a%-]+/[%a%-]+)")
@@ -63,6 +79,24 @@ local function findHeader(headers, query)
 	return nil, nil
 end
 
+local function getEncodingFromHeaders(headers)
+	local typeHeader = findHeader(headers, "content-type")
+	if typeHeader then
+		local contentType = matchContentType(typeHeader[2])
+		return contentType and lower(contentType) or nil
+	end
+end
+
+local function buildErrInfo(enum, errTrace, body, response)
+	return {
+		type = enum or FailReason.Unknown,
+		trace = errTrace,
+		body = body,
+		response = response,
+	}
+end
+
+
 
 local encoders = {
 	[Encoding.json] = json.encode,
@@ -73,73 +107,91 @@ local decoders = {
 	[Encoding.url]  = querystring.parse,
 }
 local function request(method, url, payload, encoding, headers, schema, options)
-	encoding = encoding or Encoding.json
 	headers = headers and normalizeHeaders(headers) or {}
 
-	-- Insert Content-Type header
-	if not findHeader(headers, "content-type") then
-		insert(headers, {"Content-Type", encoding})
-	end
 
 	-- Encode payload
-	if payload ~= nil and type(payload) ~= "string" then
-		payload = (encoders[encoding] or tostring)(payload)
+	local shouldEncode = (encoding ~= nil or payload ~= nil)
+
+	local encoded, encodeError
+	if shouldEncode then
+		encoded, encodeError = customPcall(encoders[encoding] or tostring, payload)
+
+		if not encoded then
+			return nil, "Bad payload: Failed to encode", buildErrInfo(
+				FailReason.BadPayload,
+				encodeError,
+				payload, nil
+			)
+		end
+	end
+
+	-- Insert encoding header
+	if encoding and not findHeader(headers, "content-type") then
+		insert(headers, {"Content-Type", encoding})
 	end
 
 
 	-- Make request
-	local success, res, body = pcall(
-		http.request, method, url, headers, payload, options
+	local res, body = customPcall(
+		http.request, method, url, headers, encoded, options
 	)
 
-
-	-- Handle errors
-	if not success then
-		return nil, "Sending request failed: " .. tostring(res), nil
+	if not res then
+		return nil, "Sending request failed", buildErrInfo(
+			FailReason.RequestError,
+			body,
+			nil, nil
+		)
 
 	elseif res.code >= 300 then
-		return nil, res.code .. ": " .. res.reason, res
+		return nil, "Request failed: " .. res.code .. ": " .. res.reason, buildErrInfo(
+			FailReason.BadResponseCode,
+			nil,
+			body, res
+		)
 	end
 
 
 	-- Decode response
-	do
-		local decoding
-		local typeHeader = findHeader(res, "content-type")
-		if typeHeader then
-			decoding = lower(matchContentType(typeHeader[2]))
-		end
+	local decoding = getEncodingFromHeaders(res)
+	local decoded, decodeErr = customPcall(decoders[decoding] or tostring, body)
 
-		local decodeSuccess
-		decodeSuccess, body = pcall(decoders[decoding] or tostring, body)
-
-		if not decodeSuccess then
-			return nil, "Invalid response body: Failed to decode", body
-		end
+	if not decoded then
+		return nil, "Bad response body: Failed to decode", buildErrInfo(
+			FailReason.BadResponseBody,
+			decodeErr,
+			body, res
+		)
 	end
 
 	-- Typecheck result
 	if schema then
-		local name, expected, actual = schema("body", body)
+		local name, expected, actual = schema("body", decoded)
+
 		if actual then
-			return nil, format(
-				"Invalid response %s: Expected %s, got %s",
-				name, expected, actual
-			), body
+			local err = format("Bad response %s: Expected %s, got %s", name, expected, actual)
+			return nil, err, buildErrInfo(
+				FailReason.InvalidResponseBody,
+				nil,
+				decoded, res
+			)
 		end
 	end
 
-	return body, res, nil
+	return decoded, res, nil
 end
 
 
 
 return {
 	request = request,
+
 	Encoding = Encoding,
+	FailReason = FailReason,
 
 	coroHttp = http,
 	json = json,
 	querystring = querystring,
-	schema = schema,
+	schema = schemaLib,
 }
