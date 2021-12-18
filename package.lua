e0a4585d55686429b9b16c1ff50ed056233d30f7
Xinshou <chairman@cloudstring.ltd>
2021-12-18 11:26:41 +0600

    v2.0.0

--- package.lua
+++ package.lua
@@ -1,6 +1,6 @@
   return {
     name = "LeXinshou/smtp-mail",
-    version = "1.5.0",
+    version = "2.0.0",
     description = "Use SMTP mail client service using Luvit",
     tags = { "smtp", "mail", "luvit" },
     license = "MIT",
