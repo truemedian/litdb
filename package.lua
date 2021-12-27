8cfb2e7af8b2317a5933ab7540c4126d2b7e99d0
Lautenschlager <tai.romani@hotmail.com>
2021-12-27 16:13:37 -0300

    v1.0.3

--- package.lua
+++ package.lua
@@ -1,5 +1,5 @@
 return {
-	version = "1.0.2",
+	version = "1.0.3",
 
 	name = "Lautenschlager-id/prepdir",
 	author = "Lautenschlager-id",
@@ -7,7 +7,6 @@ return {
 	homepage = "https://github.com/Lautenschlager-id/prepdir",
 	license = "MIT",
 
-	files = { "*.lua" },
 	dependencies = { },
 	tags = {
 		"lua",
