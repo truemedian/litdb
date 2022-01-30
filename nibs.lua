5cbef44024fcff9ed9336aec94b75bcee3fe5687
Tim Caswell <creationix@vercel.com>
2022-01-29 22:08:58 -0600

    v1.0.0

---
+++ nibs.lua
@@ -0,0 +1,6 @@
+local encode = require './encoder'
+local decode = require './decoder'
+return {
+    encode = encode,
+    decode = decode,
+}
---
+++ package.lua
@@ -0,0 +1,12 @@
+return {
+    name = "creationix/nibs",
+    version = "1.0.0",
+    homepage = "https://github.com/creationix/nibs",
+    description = "An implementation of nubs for luajit.",
+    tags = {"nibs","serialization","jit"},
+    license = "MIT",
+    author = { name = "Tim Caswell" },
+    files = {
+      "*.lua",
+    },
+  }
