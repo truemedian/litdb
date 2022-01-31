d03ab48d582f559d1006141ff5a744340e61fd4e
Quichk <74837790+DumpBotDeveloper@users.noreply.github.com>
2022-01-31 15:13:34 -0600

    v1.2.0

---
+++ package.lua
@@ -0,0 +1,13 @@
+return {
+	name = "Corotyest/lua-extentions",
+	version = "1.2.0",
+	description = "Useful functions for the Lua programming language.",
+	tags = { "jit", "fast", "util", "secure" },
+	license = "MIT",
+	author = { name = "Quichk" },
+	dependencies = {},
+	files = {
+		"**.lua",
+		"!test*"
+	}
+}
