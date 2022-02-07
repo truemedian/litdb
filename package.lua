5c7981ecfe4034a1d4d008d15c315783669b39ef
Quichk <nil>
2022-02-07 01:54:24 -0600

    v1.2.7

--- package.lua
+++ package.lua
@@ -1,6 +1,6 @@
 return {
 	name = "Corotyest/lua-extentions", -- My typo
-	version = "1.2.6",
+	version = "1.2.7",
 	description = "Useful functions for the Lua programming language.",
 	tags = { "jit", "fast", "util", "secure" },
 	license = "MIT",
