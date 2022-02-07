8fc1b978c9f2acae6ee2b69e8c03bfc234f0db52
Quichk <nil>
2022-02-07 01:04:29 -0600

    v1.0.2

--- package.lua
+++ package.lua
@@ -1,13 +1,13 @@
 return {
 	name = "Corotyest/Mixcordia",
-	version = "1.0.1",
+	version = "1.0.2",
 	description = "A bot to play your favorite songs.",
 	tags = { "music", "discordia", "coro", "youtube-dl" },
 	license = "MIT",
 	author = { name = "Quichk", email = "nil" },
 	homepage = "https://github.com/Corotyest/Mixcordia",
 	dependencies = {
-		'SinisterRectus/Discordia',
+		'SinisterRectus/discordia',
 		'Corotyest/lua-extentions'
 	},
 	files = {
