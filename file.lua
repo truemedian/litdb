454ac1f2c3d3209c81e65dee9a2d3bc7e3755a26
Quichk <nil>
2022-02-16 13:26:39 -0600

    v0.0.2

--- file.lua
+++ file.lua
@@ -1,9 +1,7 @@
 --[[lit-meta
-  name = "Corotyest/task"
-  version = "0.0.1"
-  license = "Apache 2"
-  description = "A manager for uv.new_timer..."
-  tags = {"luvit", "uv", "time"}
+	name = 'Corotyest/task'
+	version = '0.0.2'
+	author = 'Quichk'
 ]]
 
 local threads = {}
--- package.lua
+++
@@ -1,7 +0,0 @@
-return {
-	name = "Corotyest/task",
-	version = "0.0.1",
-	license = "Apache 2",
-	description = "A manager for uv.new_timer...",
-	tags = {"luvit", "uv", "time"},
-}
