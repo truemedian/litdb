b568091a27be2018ebae47e0adaa85c3ccd029e8
Quichk <nil>
2022-02-20 19:24:44 -0600

    v0.3.0

--- file.lua
+++ file.lua
@@ -1,66 +1,113 @@
 --[[lit-meta
 	name = 'Corotyest/inspect'
-	version = '0.2.0'
+	version = '0.3.0'
 ]]
 
-local format, rep = string.format, string.rep
+local format, rep, sort = string.format, string.rep, table.sort
 
-local types = {
-	['nil'] = true,
-	['table'] = true,
-	['number'] = true,
-	['boolean'] = true,
-	['userdata'] = true,
-	['function'] = true,
-}
+-- local colors = {
+-- 	['nil'] = '1;30',
+-- 	['sep'] = '1;30',
+-- 	['table'] = '1;34',
+-- 	['field'] = '0;37',
+-- 	['string'] = '0;32',
+-- 	['braces'] = '1;30',
+-- 	['thread'] = '1;35',
+-- 	['number'] = '1;33',
+-- 	['quotes'] = '1;32',
+-- 	['boolean'] = '0;33',
+-- 	['function'] = '0;35',
+-- 	['userdata'] = '1;36',
+-- } not yet
 
 local function getn(list)
 	local num = 0
-	for _ in next, list do
+	for _ in pairs(list) do
 		num = num + 1
 	end
 	return num
 end
 
-local function _format(v)
-	local type = type(v)
-	local _str = tostring(v)
+local last, getuserdata = nil, debug.getuservalue
 
-	if types[type] then
-		return _str
-	elseif type == 'string' then
-		return format('\'%s\'', _str)
-	else
-		return format('[%s].new(%s)', type, _str)
+local function isMethod(string)
+	return type(string) == 'string' and string:find('__', 1, true) == 1
+end
+
+local last1, last2
+
+local function sortfn(a, b)
+	if type(a[2]) == 'table' and not a[2] == last1 then sort(a[2], sortfn); last1 = a[2] end
+	if type(b[2]) == 'table' and not b[2] == last2 then sort(b[2], sortfn); last2 = b[2] end
+
+	if #a[1] < #b[1] then
+		return true
 	end
 end
 
-local function encode(list, tabs)
-	local n = getn(list)
+local function order(list, fn)
+	local clone = {}
+	for index, value in pairs(list) do
+		clone[#clone + 1] = { index, value }
+	end
+	
+	sort(clone, fn or sortfn)
 
-	local response = ''
-	tabs = tabs or 1
+	return next, clone
+end
 
-	for _index, _value in next, list do
-		local isTable = type(_value) == 'table'
+local function makeField(self, _index, _value, tabs)
+	local type1 = type(_value)
+	local tag = type1 == 'userdata' and tostring(_value)
+	return format('%s[\'%s\'] = %s,\n',
+		rep('\t', tabs),
+		tostring(_index),
+		type1 ~= 'table' and format(type1 == 'string' and '\'%s\'' or '%s', tostring(_value)) or
+			last == _value and 'table: self' or self:encode(_value, tabs + 1, tag)
+	)
+end
 
-		response = response .. format('%s[%s] = %s\n',
-			rep('\t', tabs),
-			_format(_index),
-			not isTable and _format(_value) or encode(_value, tabs + 1)
-		)
+local function encode(self, list, tabs, tag)
+	if last and last == list then
+		return tostring(list)
 	end
 
-	return format('{%s}', format('%s%s%s',
+	last = list
+
+	local type1 = type(list)
+	if type1 ~= 'table' then
+		if type1 == 'userdata' then return self:encode(getuserdata(list), tabs, tostring(list)) end
+		return tostring(list)
+	end
+
+	local n = getn(list)
+
+	local response, methods = '', ''
+	tabs = tabs or 1
+
+	for _, data in order(list) do
+		local _index, _value = data[1], data[2]
+		if isMethod(_index) then
+			methods = methods .. self:makeField(_index, _value, tabs)
+		else
+			response = response .. self:makeField(_index, _value, tabs)
+		end
+	end
+
+	tag = tag and format('userdata %s: ', tag:gsub('^%a*(%s+)', ''))
+
+	return format('%s{%s%s%s}',
+		tag or '',
 		n~=0 and '\n' or '',
+		#methods~=0 and methods .. '\n' or '',
 		response,
-		n~=0 and rep('\t', tabs - 1) or ''))
+		n~=0 and rep('\t', tabs - 1) or '')
 end
 
 return setmetatable({
-	types = types,
-	format = _format,
-	encode = encode
-}, {
-	__call = function(_, ...) return encode(...) end
-})
+	getn = getn,
+	colors = colors,
+	encode = encode,
+	isMethod = isMethod,
+	makeField = makeField
+}, {__call = encode})
