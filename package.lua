0d04f68423bc239057d3375b281a51f33cfddbb5
Ari Kumikaeru <phantomcrazyheart@gmail.com>
2022-03-11 08:58:09 -0500

    v0.0.1

---
+++ package.lua
@@ -0,0 +1,15 @@
+  return {
+    name = "alphafantomu/discordia-extensions",
+    version = "0.0.1",
+    description = "seperate package of discordia's extension module",
+    tags = { "discordia", "stand-alone", "luvit", "utility", "copy" },
+    license = "MIT",
+    author = { name = "Ari Kumikaeru", email = "phantomcrazyheart@gmail.com" },
+    homepage = "https://github.com/alphafantomu/discordia-extensions",
+    dependencies = {},
+    files = {
+      "**.lua",
+      "!test*"
+    }
+  }
+  
