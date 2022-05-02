f09257b318fff6dd14d4dc13cbe1e31c4e2dcd9e
Lyrthras <defelixanne@gmail.com>
2022-05-03 01:02:39 +0800

    v0.0.1

---
+++ package.lua
@@ -0,0 +1,14 @@
+return {
+  name = "Lyrth/test-package",
+  version = "0.0.1",
+  description = "Test package",
+  tags = { "lua", "lit", "luvit" },
+  license = "Apache License 2.0",
+  author = { name = "Lyrthras", email = "me@lyr.pw" },
+  homepage = "https://github.com/Lyrth/lit-test-package",
+  dependencies = {},
+  files = {
+    "**.lua",
+    "!test*"
+  }
+}
