a517ba106bc59de257f3e51a7d6bde36b61903a5
Lyrthras <defelixanne@gmail.com>
2022-05-03 17:56:00 +0800

    v0.0.2

--- package.lua
+++ package.lua
@@ -1,6 +1,6 @@
 return {
   name = "Lyrth/test-package",
-  version = "0.0.1",
+  version = "0.0.2",
   description = "Test package",
   tags = { "lua", "lit", "luvit" },
   license = "Apache License 2.0",
