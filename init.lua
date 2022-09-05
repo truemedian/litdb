ffe53eda072cc8f28778e5eb4abda38653babbfa
4keef <keef.devv@gmail.com>
2022-09-05 06:01:38 -0400

    v0.0.2

--- init.lua
+++ init.lua
@@ -1,27 +1,27 @@
-local FS = require('fs')
-local PATH  = require('path')
+local fs = require('fs')
+local env = require('env')
 
 local function split(str, sep)
   sep = sep or "%s"
   local t = {}
-  for v in string.gmatch(str, "([^"..sep.."]+)") do table.insert(t, v) end
+  for v in string.gmatch(str, "([^"..sep.."]+)")
+  do
+    table.insert(t, v)
+  end
   return t
 end
 
-local function load_env(filePath)
-  assert(
-    FS.existsSync(filePath),
-    "Invalid file path \""..filePath.."\""
-  )
-
-  local file = io.open(filePath, "r")
-
-  _ENV = {}
-  for line in file:lines() do local v = split(line, "=") _ENV[v[1]] = v[2] end
+local function load_env(path)
+  assert(fs.existsSync(path), "Invalid file path \""..path.."\"")
+  local file = io.open(path, "r")
+  for line in file:lines()
+  do
+    local v = split(line, "=")
+    env.set(unpack(v))
+  end
   file:close()
   return true
 end
-
 
 return {
   load_env = load_env
--- package.lua
+++ package.lua
@@ -1,6 +1,6 @@
   return {
     name = "4keef/Dotenv",
-    version = "0.0.1",
+    version = "0.0.2",
     description = "Parse dotenv files",
     tags = { "lua", "lit", "luvit", "dotenv", "parser" },
     license = "MIT",
