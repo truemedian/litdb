fa7ab88412ddd04edf056bb2c119ec2281f56e55
UrNightmaree <afiqquraisyzulkarnain@gmail.com>
2022-09-24 12:22:58 +0000

    v1.0.0

---
+++ package.lua
@@ -0,0 +1,18 @@
+  return {
+    name = "UrNightmaree/tluvit-core",
+    version = "1.0",
+    description = "A remade of luvit/core for Tluvit",
+    tags = { "luvit", "teal", "tluvit" },
+    license = "MIT",
+    author = 'UrNightmaree',
+    homepage = "https://github.com/UrNightmaree/tluvit-modules",
+    dependencies = {
+      'UrNightmaree/tluvit'
+    },
+    files = {
+      "**.tl",
+      "**.lua",
+      "!test*"
+    }
+  }
+  
