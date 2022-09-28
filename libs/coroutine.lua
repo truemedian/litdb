dccd9c2cd6c0c81b6de732be0534ea9746c85e06
Nameless <truemedian@gmail.com>
2022-09-28 13:01:26 -0500

    v1.1.0

--- libs/coroutine.lua
+++ libs/coroutine.lua
@@ -1,37 +1,38 @@
 ---Extensions to the Lua standard coroutine library.
 ---@module extensions.coroutine
 ---@alias ext_coro
+local pack = table.pack
 local resume, running = coroutine.resume, coroutine.running
 local traceback = debug.traceback
 
 local function pack_vararg(success, err, ...)
-    return success, err, {...}
+	return success, err, pack(...)
 end
 
 local ext_coro = {}
 
 for k, v in pairs(coroutine) do
-    ext_coro[k] = v
+	ext_coro[k] = v
 end
 
----Properly asserts a coroutine resume in order to get the correct traceback on error. Consumes the first two returns from resume (success and err)
+---Properly asserts a coroutine resume in order to get the correct traceback on error. Consumes the first return from resume (the success boolean)
 ---and returns the rest.
 ---@param thread thread
 ---@param[opt] ... any
 ---@return ...
 function ext_coro.assertresume(thread, ...)
-    local success, err, args = pack_vararg(resume(thread, ...))
-    if not success then
-        error(traceback(thread, err), 0)
-    end
+	local success, err, args = pack_vararg(resume(thread, ...))
+	if not success then
+		error(traceback(thread, err), 0)
+	end
 
-    return unpack(args)
+	return err, unpack(args, 1, args.n)
 end
 
 ---Returns whether or not the function was run inside of a coroutine. Returns false if it is on the main thread.
 ---@return boolean
 function ext_coro.isthread()
-    return not select(2, running())
+	return not select(2, running())
 end
 
 return ext_coro
--- libs/table.lua
+++ libs/table.lua
@@ -7,59 +7,46 @@ local random = math.random
 local ext_table = {}
 
 for k, v in pairs(table) do
-    ext_table[k] = v
+	ext_table[k] = v
 end
 
 ---Returns a new table with a single layer of keys-values copied.
 ---@param tbl table
 ---@return table
 function ext_table.copy(tbl)
-    local new = {}
+	local new = {}
 
-    for k, v in pairs(tbl) do
-        new[k] = v
-    end
+	for k, v in pairs(tbl) do
+		new[k] = v
+	end
 
-    return new
+	return new
 end
 
 ---Returns a new table with a all layers of keys-values copied. Tables are copied recursively.
 ---@param tbl table
 ---@return table
 function ext_table.deepcopy(tbl)
-    local new = {}
+	local new = {}
 
-    for k, v in pairs(tbl) do
-        new[k] = type(v) == 'table' and ext_table.deepcopy(v) or v
-    end
+	for k, v in pairs(tbl) do
+		new[k] = type(v) == 'table' and ext_table.deepcopy(v) or v
+	end
 
-    return new
+	return new
 end
 
 ---Returns the number of keys in the table.
 ---@param tbl table
 ---@return number
 function ext_table.count(tbl)
-    local n = 0
+	local n = 0
 
-    for _ in pairs(tbl) do
-        n = n + 1
-    end
+	for _ in pairs(tbl) do
+		n = n + 1
+	end
 
-    return n
-end
-
----Returns the number of keys in the table by recursively looking into tables.
----@param tbl table
----@return number
-function ext_table.deepcount(tbl)
-    local n = 0
-
-    for _, v in pairs(tbl) do
-        n = type(v) == 'table' and n + ext_table.deepcount(v) or n + 1
-    end
-
-    return n
+	return n
 end
 
 ---Returns a new table with new key-value pairs sourced from the map function.
@@ -69,19 +56,19 @@ end
 ---@param fn function
 ---@return table
 function ext_table.map(tbl, fn)
-    local new = {}
+	local new = {}
 
-    for k, v in pairs(tbl) do
-        local new_v, new_k = fn(v, k)
+	for k, v in pairs(tbl) do
+		local new_v, new_k = fn(v, k)
 
-        if new_k == nil then
-            new[k] = new_v
-        else
-            new[new_k] = new_v
-        end
-    end
+		if new_k == nil then
+			new[k] = new_v
+		else
+			new[new_k] = new_v
+		end
+	end
 
-    return new
+	return new
 end
 
 ---Returns a new table with only key-value pairs that cause `fn` to return true.
@@ -90,39 +77,60 @@ end
 ---@param fn function
 ---@return table
 function ext_table.filter(tbl, fn)
-    local new = {}
+	local new = {}
 
-    for k, v in pairs(tbl) do
-        if fn(v, k) then
-            new[k] = v
-        end
-    end
+	for k, v in pairs(tbl) do
+		if fn(v, k) then
+			new[k] = v
+		end
+	end
 
-    return new
+	return new
+end
+
+---Iterates over the array portion of a table in order, calling `fn` on each value, passing in the return value from the previous element.
+---If `initial` is not provided, the first element of the array is used, and the iteration starts at the second element.
+---The reduce function has a signature of: `fn(previous, value, index?) boolean`
+---@param tbl table
+---@param fn fun(previous: any, value: any, index?: integer): boolean
+---@param initial any
+---@return any
+function ext_table.reduce(tbl, fn, initial)
+	local reduced = initial
+
+	for i, v in ipairs(tbl) do
+		if i == 1 and reduced == nil then
+			reduced = v
+		else
+			reduced = fn(reduced, v, i)
+		end
+	end
+
+	return reduced
 end
 
 ---Reverses the contents of the array.
+---If `dest` is provided, the source array will not be modified
 ---@param tbl table
-function ext_table.reverse(tbl)
-    local i, n = 1, #tbl
-
-    while i < n do
-        tbl[i], tbl[n] = tbl[n], tbl[i]
-        i, n = i + 1, n - 1
-    end
-end
-
----Returns a new array with the reversed contents of the original.
----@param tbl table
+---@param dest? table
 ---@return table
-function ext_table.reversed(tbl)
-    local new = {}
+function ext_table.reverse(tbl, dest)
+	if dest then
+		for i = 1, #tbl do
+			dest[i] = tbl[#tbl - i + 1]
+		end
 
-    for i = 1, #tbl do
-        new[i] = tbl[#tbl - i + 1]
-    end
+		return dest
+	else
+		local i, n = 1, #tbl
 
-    return new
+		while i < n do
+			tbl[i], tbl[n] = tbl[n], tbl[i]
+			i, n = i + 1, n - 1
+		end
+
+		return tbl
+	end
 end
 
 ---Shifts every index after `index` in the table to the right by `count`.
@@ -130,17 +138,17 @@ end
 ---@param index number
 ---@param count number
 function ext_table.shift(tbl, index, count)
-    local i = #tbl
+	local i = #tbl
 
-    while i >= index do
-        tbl[i + count] = tbl[i]
+	while i >= index do
+		tbl[i + count] = tbl[i]
 
-        if i < index + count then
-            tbl[i] = nil
-        end
+		if i < index + count then
+			tbl[i] = nil
+		end
 
-        i = i - 1
-    end
+		i = i - 1
+	end
 end
 
 ---Returns a slice of the table, works similarly to `string.sub` except on a table.
@@ -150,25 +158,53 @@ end
 ---@param[opt] step number
 ---@return table
 function ext_table.slice(tbl, start, stop, step)
-    local new = {}
+	local new = {}
 
-    for i = start or 1, stop or #tbl, step or 1 do
-        insert(new, tbl[i])
-    end
+	for i = start or 1, stop or #tbl, step or 1 do
+		insert(new, tbl[i])
+	end
 
-    return new
+	return new
 end
 
----Works identically to `memcpy` in C. copies all of src into dest starting at index.
----@param dest table
----@param src table
----@param index number
-function ext_table.memcpy(dest, src, index)
-    index = index - 1
+---Appends all elements of `b` into `a`. Does not consider non-array elements.
+---@param a table
+---@param b table
+---@return table
+function ext_table.join(a, b)
+	for _, v in ipairs(b) do
+		insert(a, v)
+	end
 
-    for i = 1, #src do
-        dest[index + i] = src[i]
-    end
+	return a
+end
+
+---Merges all key-value pairs of `b` into `a`. Will overwrite any existing keys.
+---@param a table
+---@param b table
+---@return table
+function ext_table.merge(a, b)
+	for k, v in pairs(b) do
+		a[k] = v
+	end
+
+	return a
+end
+
+---This works exactly like [`table.move`](https://www.lua.org/manual/5.3/manual.html#pdf-table.move) introduced in Lua 5.3.
+---@param src table
+---@param first integer
+---@param stop integer
+---@param src_offset integer
+---@param dest table
+function ext_table.move(src, first, stop, src_offset, dest)
+	dest = dest or src
+
+	for i = 0, stop - first do
+		dest[src_offset + i] = src[first + i]
+	end
+
+	return dest
 end
 
 ---Removes the value at the index from an array and replaces it with the item at the end of the array.
@@ -176,11 +212,11 @@ end
 ---@param i number
 ---@return any
 function ext_table.swapremove(tbl, i)
-    local value = tbl[i]
+	local value = tbl[i]
 
-    tbl[i] = tbl[#tbl]
+	tbl[i] = tbl[#tbl]
 
-    return value
+	return value
 end
 
 ---Looks for a specific value in a table and returns the key it was first found at.
@@ -188,68 +224,105 @@ end
 ---@param value any
 ---@return any|nil
 function ext_table.search(tbl, value)
-    for k, v in pairs(tbl) do
-        if v == value then
-            return k
-        end
-    end
+	for k, v in pairs(tbl) do
+		if v == value then
+			return k
+		end
+	end
 
-    return nil
+	return nil
 end
 
 ---Returns an array of keys available in the table.
 ---@param tbl table
 ---@return table
 function ext_table.keys(tbl)
-    local new = {}
+	local new = {}
 
-    for k in pairs(tbl) do
-        insert(new, k)
-    end
+	for k in pairs(tbl) do
+		insert(new, k)
+	end
 
-    return new
+	return new
 end
 
 ---Returns an array of values available in the table.
 ---@param tbl table
 ---@return table
 function ext_table.values(tbl)
-    local new = {}
+	local new = {}
 
-    for _, v in pairs(tbl) do
-        insert(new, v)
-    end
+	for _, v in pairs(tbl) do
+		insert(new, v)
+	end
 
-    return new
+	return new
 end
 
 ---Returns whether or not the table is empty.
 ---@param tbl table
 ---@return boolean
 function ext_table.isempty(tbl)
-    return not next(tbl)
+	return (next(tbl)) == nil
+end
+
+---Shuffles a table in place.
+---@param tbl table
+---@return table
+function ext_table.shuffle(tbl)
+	for i = #tbl, 1, -1 do
+		local j = random(i)
+		tbl[i], tbl[j] = tbl[j], tbl[i]
+	end
+
+	return tbl
 end
 
 ---Returns a random key, value index from an array.
 ---@param tbl table
 ---@return any, any
 function ext_table.randomipair(tbl)
-    local i = random(#tbl)
-    return i, tbl[i]
+	local i = random(#tbl)
+	return i, tbl[i]
+end
+
+---Returns a random key, value index from an array.
+---@param tbl table
+---@return any, any
+function ext_table.randomvalue(tbl)
+	local values = ext_table.values(tbl)
+	return values[random(#values)]
 end
 
 ---Returns a random key, value index from a table.
 ---@param tbl table
 ---@return any, any
 function ext_table.randompair(tbl)
-    local rand = random(ext_table.count(tbl))
-    local n = 0
-    for k, v in pairs(tbl) do
-        n = n + 1
-        if n == rand then
-            return k, v
-        end
-    end
+	local rand = random(ext_table.count(tbl))
+	local n = 0
+	for k, v in pairs(tbl) do
+		n = n + 1
+		if n == rand then
+			return k, v
+		end
+	end
+end
+
+---Fills a table from 1 to `len` with `value`.
+---@param tbl table
+---@param value any
+---@param len? integer
+---@param start? integer
+---@return table
+function ext_table.fill(tbl, value, len, start)
+	len = len or #tbl
+	start = start or 1
+
+	for i = 1, len do
+		tbl[i] = value
+	end
+
+	return tbl
 end
 
 return ext_table
