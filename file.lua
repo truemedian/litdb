0a683dd9028461215a02808e2202c71ff33bf705
Corotyest <aedipaulov@gmail.com>
2022-11-11 13:56:32 -0600

    v1.0.0

--- file.lua
+++ file.lua
@@ -1,19 +1,23 @@
 --[[lit-meta
 	name = 'Corotyest/content'
-	version = '0.2.3'
+	version = '1.0.0'
 	dependencies = { 'Corotyest/lua-extensions', 'Corotyest/inspect' }
 ]]
 
-local inspect = require 'inspect'
-local extensions = require 'lua-extensions'(true)
-local string, table = extensions.string, extensions.table
+-- Semi-Rework. Next to generate a documentation.
 
-local getudata, setudata = debug.getuservalue, debug.setuservalue
-local f_type, open, close = io.type, io.open, io.close
+local inspect = require 'inspect'
+local extensions = require 'lua-extensions' ()
+local string, table = extensions.string or string, extensions.table or table
+
 local remove, execute = os.remove, os.execute
-local format, sfind, match, _split = string.format, string.find, string.match, string.split
 local concat, sinsert = table.concat, table.sinsert
-local _error = 'incorrect argument #%s for %s (%s expected got %s)'
+local getudata, setudata = debug.getuservalue, debug.setuservalue
+local typeof, open, close = io.type, io.open, io.close
+local format, sfind, match, _split = string.format, string.find, string.match, string.split
+
+local arg = 'incorrect argument #%s for %s (%s expected got %s)'
+local request = '%s%srequest'
 
 local haswindows = jit and jit.arch == 'Windows' or package.path:match('\\') and true
 local prefix = haswindows and '\\' or '/'
@@ -21,7 +25,7 @@ local prefix = haswindows and '\\' or '/'
 local function split(value)
 	local type1 = type(value)
 	if type1 ~= 'string' then
-		return nil, format(_error, 1, 'split', 'string', type1)
+		return nil, format(arg, 1, 'split', 'string', type1)
 	end
 
 	-- different matchs for get correct values
@@ -34,7 +38,7 @@ end
 local function attach(path, ...)
 	local type1 = type(path)
 	if type1 ~= 'string' then
-		return nil, format(_error, 1, 'attach', 'string', type1)
+		return nil, format(arg, 1, 'attach', 'string', type1)
 	end
 
 	local has = sfind(path, '/', 1, true) ~= 1
@@ -50,20 +54,20 @@ local function attach(path, ...)
 	return concat(response, prefix)
 end
 
-local function edit_dir(self, path, deldir)
+local function editDir(self, path, deldir)
 	local type1, type2 , type3 = type(self), type(path), type(deldir)
 	if type1 ~= 'table' then
-		return nil, format(_error, 'self', 'edit_dir', type1)
+		return nil, format(arg, 'self', 'editDir', type1)
 	elseif type2 ~= 'string' then
-		return nil, format(_error, 1, 'edit_dir', 'string', type2)
+		return nil, format(arg, 1, 'editDir', 'string', type2)
 	elseif deldir and type3 ~= 'boolean' then
-		return nil, format(_error, 2, 'edit_dir', 'boolean', type3)
+		return nil, format(arg, 2, 'editDir', 'boolean', type3)
 	end
 
 	local path = attach(path)
 
 	if path and #path ~= 0 then
-		local endpoint = format(self.request, path, prefix)
+		local endpoint = format(request, path, prefix)
 		local response = open(endpoint, 'w')
 
 		if not response then
@@ -81,13 +85,13 @@ local function isFile(file)
 		return false
 	end
 
-	return f_type(file) ~= nil or sfind(tostring(file), 'file', 1, true) == 1
+	return typeof(file) ~= nil or sfind(tostring(file), 'file', 1, true) == 1
 end
 
-local function file_info(file, data)
+local function setFileInfo(file, data)
 	local type1 = type(file)
 	if not isFile(file) then
-		return nil, format(_error, 1, 'file_info', 'userdata', type1)
+		return nil, format(arg, 1, 'setFileInfo', 'userdata', type1)
 	end
 
 	data = type(data) == 'table' and data or { __name = data }
@@ -105,23 +109,25 @@ local function file_info(file, data)
 end
 
 local handles = { }
-local _handle = { }
 
-function _handle.open(self, filename)
+local function openFile(self, filename)
 	filename = filename or self.filename
 
-	local input = open(filename, 'r')
-	if not input then if open(filename, 'w') then input = open(filename, 'r') end end
+    local input = open(filename, 'r')
+	if not input and open(filename, 'w') then
+		input = open(filename, 'r')
+	end
 
-	file_info(input, filename)
+	setFileInfo(input, filename)
 
 	self.handle.input = input
 	self.handle.output = {
 		write = function(self, ...)
 			local file = open(filename, 'w')
-			local success, error = pcall(file.write, file, ...); file:close()
+			local success, error = pcall(file.write, file, ...)
 
 			if success then
+				file:close()
 				return success
 			else
 				return nil, error
@@ -133,31 +139,33 @@ function _handle.open(self, filename)
 	return self
 end
 
-function _handle.close(self)
+local function close(self)
 	return self.handle.input:close()
 end
 
-function _handle.reboot(self)
-	if self:close() then
+local function reboot(self)
+	if self.close and self:close() then
 		return self:open()
 	end
 end
 
-function _handle.content(self, ...)
+local function content(self, ...)
 	local file = self.handle.input
 
 	local lines = { }
 	for line in file:lines() do
 		lines[#lines + 1] = line
 	end
-	
+
 	local data = concat(lines, '\n')
 	local success, chunck = pcall(load(data, file.__name, nil, _G), ...)
 
-	return success and chunck or data, self:reboot()
+	self:reboot()
+
+	return success and chunck or data
 end
 
-function _handle.write(self, options, ...)
+local function write(self, options, ...)
 	local file = self.handle.output
 
 	local type1 = type(options)
@@ -169,7 +177,7 @@ function _handle.write(self, options, ...)
 	return file:write(self.extension == 'lua' and setret and 'return ' or '', unpack(base))
 end
 
-function _handle.apply(self, options)
+local function apply(self, options)
 	local key, value = options.key, options.value
 
 	local module = self:content()
@@ -191,70 +199,90 @@ function _handle.apply(self, options)
 	return self:write(options, inspect.encode(module))
 end
 
-local _remove = remove
-
-function _handle.remove(self)
+local function delete(self)
 	local filename = self.filename
-	self:close(); handles[self] = nil; return _remove(filename)
-end
 
-local info = debug.getinfo
+	local success = self:close()
+	if not success then
+		return nil
+	end
+
+	handles[self] = nil
+
+	return remove(filename)
+end
 
 local function newHandle(self, file)
 	local type1, type2 = type(self), type(file)
 	if type1 ~= 'table' then
-		return nil, format(_error, 'self', 'neHandle', 'table', type1)
+		return nil, format(arg, 'self', 'neHandle', 'table', type1)
 	elseif type2 ~= 'string' then
 		if not self.isFile(file) then
-			return nil, format(_error, 1, 'newHandle', 'string/userdata', type2)
+			return nil, format(arg, 1, 'newHandle', 'string/userdata', type2)
 		end
 	end
 
 	local pathname, filename, extension = self.split(file)
 
-	if extension ~= 'lua' then return error('currently only supporting lua files', 2) end
+	if extension ~= 'lua' then
+		return error('currently only supporting lua files', 2)
+	end
 
 	local meta = { }
+
 	local props = {
 		handle = { },
 		pathname = pathname,
 		filename = pathname and (pathname .. self.prefix .. filename) or filename,
 		extension = extension,
+		-- Functions
+		open = openFile,
+		close = close,
+		apply = apply,
+		write = write,
+		delete = delete,
+		reboot = reboot,
+		content = content,
 	}
 
 	function meta.__pairs()
-		return next, _handle, nil
+		local index, value
+		return function()
+			index, value = next(props, index)
+			return index, value
+		end
 	end
 
-	function meta.__index(self, k)
-		if not handles[self] then return error('this handle is removed', 2) end
+	function meta:__index(key)
+		if not handles[self] then
+			return error('this handle is removed', 2)
+		end
 
-		local value = rawget(_handle, k)
-		if type(value) == 'function' then
+		local value = props[key]
+
+		if type(value) ~= 'function' then
+			return value
+		else
 			return function(self, ...)
 				local type1 = type(self)
 				if type1 ~= 'table' then
-					return nil, format(_error, 'self', k, 'table', type1)
+					return nil, format(arg, 'self', key, 'table', type1)
 				end
+
 				return value(self, ...)
 			end
-		else
-			return props[k]
 		end
 	end
 
-	function meta.__newindex(self, k, v)
-		local _info = info(2)
-
-		local _, name = split(_info.source)
-		if self[k] and name ~= 'content.lua' then
-			return error('attempt to index a protected table', 2)
+	function meta:__newindex(key, value)
+		if self[key] then
+			return error('attempt to overwrite a protected value', 2)
 		end
 
-		return rawset(props, k, v)
+		props[key] = value
 	end
 
-	function meta.__tostring(self)
+	function meta:__tostring()
 		return self.filename
 	end
 
@@ -265,18 +293,18 @@ local function newHandle(self, file)
 end
 
 local function isHandle(handle)
-	return handles[handle]
+	return handles[handle] == true
 end
 
 return {
 	split = split,
-	isFile = isFile,
 	attach = attach,
+	isFile = isFile,
 	prefix = prefix,
-	request = '%s%srequest',
+	editDir = editDir,
 	isHandle = isHandle,
-	edit_dir = edit_dir,
-	file_info = file_info,
 	newHandle = newHandle,
+
 	haswindows = haswindows,
+	setFileInfo = setFileInfo,
 }
