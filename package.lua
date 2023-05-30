0372cd8f09b1efecaa34de51075ef94e7576066c
Tim Caswell <creationix@vercel.com>
2023-05-30 18:03:31 +0000

    v2.1.1

--- package.lua
+++ package.lua
@@ -1,6 +1,6 @@
 return {
     name = "creationix/nibs",
-    version = "2.1.0",
+    version = "2.1.1",
     dependencies = {},
     description = "Nibs and Tibs serialization formats.",
     tags = { "lua", "lit", "luvit", "tibs", "nibs", "serialization" },
