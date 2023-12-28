ebf90bd1ba8045d29dbee0df2d4992a309640cea
qwreey75 <qwasreey74@gmail.com>
2023-12-28 20:52:24 +0000

    v9.0.0

--- init.lua
+++ init.lua
@@ -1,1 +1,13 @@
-return require "./promise"
+  --[[lit-meta
+    name = "qwreey75/promise"
+    obsolete = true
+    version = "9.0.0"
+    description = "obsoleted"
+    tags = { "lua", "lit", "luvit" }
+    license = "MIT"
+    author = { name = "qwreey75", email = "me@qwreey.moe" }
+    dependencies = {}
+    homepage = "https://github.com/qwreey"
+  ]]
+return {}
+
