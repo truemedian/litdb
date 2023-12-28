45880fc23cb6bc570ec55b8aad5263d7dee7032e
qwreey75 <qwasreey74@gmail.com>
2023-12-28 20:53:39 +0000

    v9.0.0

--- init.lua
+++ init.lua
@@ -1,1 +1,13 @@
-return require "./mutex"
+  --[[lit-meta
+    name = "qwreey75/mutex"
+    obsolete = true
+    version = "9.0.0"
+    description = "obsoleted"
+    tags = { "lua", "lit", "luvit" }
+    license = "MIT"
+    author = { name = "qwreey75", email = "me@qwreey.moe" }
+    dependencies = {}
+    homepage = "https://github.com/qwreey"
+  ]]
+return {}
+
