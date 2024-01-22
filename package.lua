9fa5d129c8878235eface225dcb37dd224b8862e
Richy Z <64844585+Richy-Z@users.noreply.github.com>
2024-01-22 18:08:11 +0000

    v0.1.3

--- package.lua
+++ package.lua
@@ -1,6 +1,6 @@
 return {
     name = "Richy-Z/lua-edulink",
-    version = "0.1.2",
+    version = "0.1.3",
     description = "EduLink One API integration for Luvit",
     tags = {"edulink", "api", "school", "education", "uk", "england", "scotland", "wales", "ireland"},
     license = "MIT",
@@ -17,8 +17,12 @@ return {
         "creationix/coro-http"
     },
     files = {
-        "**.lua",
+        --[["**.lua",
         "!privateTests*",
-        "!tests*"
+        "!tests*",
+        "!.github*"
+        ]]
+        "main.lua",
+        "package.lua"
     }
 }
