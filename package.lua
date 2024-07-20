8627d4de74e43c6bbf41f7b3c0f3179554ececb5
NickIsADev <nickexe901@gmail.com>
2024-07-20 16:38:05 -0400

    v0.0.6

--- package.lua
+++ package.lua
@@ -1,6 +1,6 @@
   return {
     name = "NickIsADev/erlua",
-    version = "0.0.5",
+    version = "0.0.6",
     description = "A library providing dynamic ratelimiting, custom functions, and easy access to the ER:LC API.",
     license = "MIT",
     files = {
@@ -8,6 +8,8 @@
     },
     dependencies = {
         "creationix/coro-http",
+	"luvit/json",
+	"luvit/secure-socket"
     }
   }
   
