b801e290536b58c02d7ce38ce5beac16b3a1a236
NickIsADev <nickexe901@gmail.com>
2024-07-20 14:56:42 -0400

    v0.0.3

--- package.lua
+++ package.lua
@@ -1,10 +1,13 @@
   return {
     name = "NickIsADev/erlua",
-    version = "0.0.2",
+    version = "0.0.3",
     description = "A library providing dynamic ratelimiting, custom functions, and easy access to the ER:LC API.",
     license = "MIT",
     files = {
       "*.lua"
+    },
+    dependencies = {
+        "creationix/coro-http",
     }
   }
   
