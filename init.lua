fd5ba3dba64032a8c6e41d4b32edd85767816420
NickIsADev <nickexe901@gmail.com>
2024-07-22 09:13:46 -0400

    v0.1.5

--- init.lua
+++ init.lua
@@ -23,7 +23,32 @@ local function find(tbl, tofind)
 	return nil
 end
 
+local find = string.find
+local sub = string.sub
+
 local function split(str, delim)
+	local ret = {}
+	if not str then
+		return ret
+	end
+	if not delim or delim == '' then
+		for c in string.gmatch(str, '.') do
+			table.insert(ret, c)
+		end
+		return ret
+	end
+	local n = 1
+	while true do
+		local i, j = find(str, delim, n)
+		if not i then break end
+		table.insert(ret, sub(str, n, i - 1))
+		n = j + 1
+	end
+	table.insert(ret, sub(str, n))
+	return ret
+end
+
+function string.split(str, delim)
 	local ret = {}
 	if not str then
 		return ret
