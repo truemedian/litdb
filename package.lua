048b9a1f5df4566a1d9c648111f5651b3bedf9bc
mrentrasilofc <mrentrasilofc>
2024-10-07 16:59:03 -0300

    v0.0.1

---
+++ package.lua
@@ -0,0 +1,14 @@
+return {
+	name = 'mrentrasilofc/http',
+	version = '0.0.1',
+	homepage = 'https://github.com/mrentrasilofc/http',
+	dependencies = {
+		'luvit/secure-socket@1.2.2',
+        'luvit/require',
+        'luvit/json'
+	},
+	tags = {'http', 'web', 'api'},
+	license = 'MIT',
+	author = 'mrentrasilofc',
+	files = {'**.lua'}
+}
