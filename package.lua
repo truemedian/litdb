08a08ed53e0e07168965cf6969fe36730bd8ae81
Lautenschlager <tai.romani@hotmail.com>
2025-01-20 22:32:20 -0300

    v9.0.4

--- package.lua
+++ package.lua
@@ -1,5 +1,5 @@
 return {
-	version = "9.0.3",
+	version = "9.0.4",
 
 	name = "Lautenschlager-id/transfromage",
 	author = "Lautenschlager-id",
@@ -7,7 +7,7 @@ return {
 	homepage = "https://github.com/Lautenschlager-id/transfromage",
 	license = "MIT",
 
-	files = { "*.lua" },
+	files = { "*.lua", "libs/*.lua" },
 	dependencies = {
 		"creationix/base64",
 		"creationix/coro-http",
