815792bedd64200a7c6f477837c4d7cad04a30a5
Richard Ziupsnys <hello@richy.lol>
2025-05-25 16:22:27 +0000

    v0.2.1

--- file.lua
+++ file.lua
@@ -1,15 +1,13 @@
 --[[lit-meta
     name = "Richy-Z/clock"
-    version = "0.1.0"
+    version = "0.2.1"
     dependencies = {}
     description = "A simple library to get precise UNIX time and other utility functions"
-    tags = { "clock", "time", "unix", "ISO", "timestamp", "ffi" }
+    tags = { "clock", "time", "unix", "iso8601", "timestamp", "ffi", "milliseconds" }
     license = "Apache 2.0"
     author = { name = "Richard Ziupsnys", email = "hello@richy.lol" }
     homepage = "https://github.com/Richy-Z/luvit-batteries"
   ]]
-
-local clock = {}
 
 local ffi = require("ffi")
 
@@ -29,7 +27,7 @@ ffi.cdef [[
       };
   ]]
 
-function clock.epoch()
+local function epoch()
     local timespec = ffi.new("struct timespec")
     local CLOCK_REALTIME = ffi.C.CLOCK_REALTIME
 
@@ -41,11 +39,67 @@ function clock.epoch()
     return tonumber(timespec.tv_sec) + tonumber(timespec.tv_nsec) / 1e9
 end
 
--- TODO: maybe use our own epoch function for nanoseconds in this ISO?
-function clock.ISO()
-    local date = os.date("!*t")
-    return string.format("%04d-%02d-%02dT%02d:%02d:%02d.000Z",
-        date.year, date.month, date.day, date.hour, date.min, date.sec)
+local function milliseconds()
+    return epoch() * 1000
 end
 
-return clock
+local function iso8601()
+    local now = epoch()
+    local seconds = math.floor(now)
+    local millis = math.floor((now - seconds) * 1000)
+
+    local date = os.date("!*t", seconds)
+    return string.format("%04d-%02d-%02dT%02d:%02d:%02d.%03dZ",
+        date.year, date.month, date.day, date.hour, date.min, date.sec, millis)
+end
+
+local function iso8601_parse(str)
+    local year, month, day, hour, min, sec, millis = str:match(
+        "(%d%d%d%d)%-(%d%d)%-(%d%d)T(%d%d):(%d%d):(%d%d)%.(%d+)Z"
+    )
+
+    if not year then -- fallback without milliseconds
+        year, month, day, hour, min, sec = str:match(
+            "(%d%d%d%d)%-(%d%d)%-(%d%d)T(%d%d):(%d%d):(%d%d)Z"
+        )
+        millis = 0
+    end
+
+    if not year then
+        return nil, "Invalid ISO 8601 format"
+    end
+
+    local t = {
+        year = tonumber(year),
+        month = tonumber(month),
+        day = tonumber(day),
+        hour = tonumber(hour),
+        min = tonumber(min),
+        sec = tonumber(sec),
+        isdst = false, -- avoids daylight saving time offset errors etc
+    }
+
+    -- force UTC interpretation
+    -- forgetting to implement UTC support was a costly mistake..
+    local localtime = os.time(t)
+    local offset = os.difftime(os.time(os.date("!*t", localtime)), localtime) ---@diagnostic disable-line: param-type-mismatch
+    local utcTime = localtime + offset
+
+    return utcTime + tonumber(millis) / 1000
+end
+
+-- tests
+assert(math.floor(iso8601_parse("2025-05-27T23:45:12.123Z")) == 1748389512) ---@diagnostic disable-line: param-type-mismatch
+-- p(iso8601_parse("2025-05-27T23:45:12.123Z"))
+
+p(iso8601_parse("2025-05-27T23:45:12.123Z"))
+
+return {
+    epoch = epoch,
+    now = epoch, -- alias for epoch
+
+    milliseconds = milliseconds,
+
+    iso8601 = iso8601,
+    iso8601_parse = iso8601_parse
+}
