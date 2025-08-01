e6aea3149f636bcd1db4de80b4d8fbd8a6ca0182
Bilal2453 <belal2453@gmail.com>
2025-08-01 05:48:01 +0300

    v1.3.0

---
+++ package.lua
@@ -0,0 +1,16 @@
+-- MIT, Copyright (C) 2014 by Leaf Corcoran
+
+return {
+  name = "Bilal2453/etlua",
+  version = "1.3.0",
+  description = "Embedded templates for Lua",
+  tags = { "SSR", "server side rendering", "templates", "template", "html", "etlua", "render" },
+  license = "MIT",
+  author = { name = "Bilal2453", email = "belal2453@gmail.com" },
+  homepage = "https://github.com/Bilal2453/lit-etlua",
+  dependencies = {},
+  files = {
+    "**.lua",
+    "!test*"
+  }
+}
