a349ddb91286ebd44b40353f2ea16c94ac905684
Alekssandher <94875660+Alekssandher@users.noreply.github.com>
2025-08-06 08:51:30 -0300

    v2.0.0

---
+++ package.lua
@@ -0,0 +1,16 @@
+return {
+  name = "alekssandher/discordia-slash",
+  version = "2.0.0",
+  description = "Slash command extension for Discordia (fork)",
+  tags = { "discord", "bot", "discordia", "slash", "discordia slash fork", "discordia slash" },
+  license = "MIT",
+  author = "Alekssandher",
+  homepage = "https://github.com/alekssandher/discordia-slash",
+  dependencies = {
+    "SinisterRectus/discordia",
+    "Bilal2453/discordia-interactions"
+  },
+  file = {
+    "*.lua"
+  }
+}
