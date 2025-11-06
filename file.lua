656af3689d74e35eb0c22519d79aeaca3d0a6cdb
code-nuage <eloi.hariot@protonmail.com>
2025-11-06 17:37:00 +0100

    v0.0.1

---
+++ file.lua
@@ -0,0 +1,76 @@
+--[[lit-meta
+    name = "code-nuage/direct-verbosity"
+    version = "0.0.1"
+    homepage = "https://github.com/code-nuage/direct/blob/main/direct-verbosity.lua"
+    description = "The verbosity plugin of the direct web microframework."
+    tags = { "direct", "plugin" }
+    license = "MIT"
+    author = { name = "code-nuage" }
+]]
+
+--+                  +--
+--  direct-verbosity  --
+--  @code-nuage       --
+--+                  +--
+
+local M = {}
+
+local colors = {
+    ["RESET"] = "\27[0m",
+    ["BOLD"] = "\27[1m",
+    ["DIM"] = "\27[2m",
+    ["ITALIC"] = "\27[3m",
+    ["UNDERLINE"] = "\27[4m",
+
+    ["RED"] = "\27[31m",
+    ["GREEN"] = "\27[32m",
+    ["YELLOW"] = "\27[33m",
+    ["BLUE"] = "\27[34m",
+    ["MAGENTA"] = "\27[35m",
+    ["CYAN"] = "\27[36m",
+    ["GRAY"] = "\27[90m"
+}
+
+local method_colors = {
+    ["GET"] = colors["GREEN"],
+    ["POST"] = colors["YELLOW"],
+    ["PUT"] = colors["BLUE"],
+    ["PATCH"] = colors["BLUE"],
+    ["DELETE"] = colors["RED"],
+    ["OPTIONS"] = colors["MAGENTA"],
+    ["HEAD"] = colors["GREEN"]
+}
+
+local code_colors = {
+    [1] = colors["BLUE"],
+    [2] = colors["GREEN"],
+    [3] = colors["MAGENTA"],
+    [4] = colors["RED"],
+    [5] = colors["RED"]
+}
+
+local function get_code_color(code)
+    return code_colors[math.floor(code / 100)] or colors["GRAY"]
+end
+
+function M.on_start(host, port)
+    print(string.format(
+        colors["BOLD"] .. colors["BLUE"] .. "--+          +--\n" ..
+        colors["RESET"] .. "Started @ " .. colors["BOLD"] .. colors["GREEN"] .. "%s:%d\n" ..
+        colors["BOLD"] .. colors["BLUE"] .. "--+          +--\n",
+        host,
+        port))
+end
+
+function M.on_request(req, res)
+    print(string.format(
+        colors["RESET"] .. "Path: " .. colors["BOLD"] .. colors["BLUE"] .. "%s\n" ..
+        colors["RESET"] .. "Method: " .. colors["BOLD"] .. (method_colors[req:get_method()] or colors["GRAY"]) .. "%s\n" ..
+        colors["RESET"] .. "Code: " .. colors["BOLD"] .. get_code_color(res:get_code()) .. "%d\n" ..
+        colors["BOLD"] .. colors["BLUE"] .. "--+          +--",
+        req:get_path(),
+        req:get_method(),
+        res:get_code()))
+end
+
+return M
