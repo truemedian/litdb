fdfbe6df74252e37c7305c8c202ab8fb0683a469
code-nuage <eloi.hariot@protonmail.com>
2025-11-28 17:29:49 +0100

    v0.1.1

--- file.lua
+++ file.lua
@@ -1,11 +1,12 @@
 --[[lit-meta
     name = "code-nuage/direct-publicize"
-    version = "0.1.0"
+    version = "0.1.1"
     homepage = "https://github.com/code-nuage/direct/blob/main/direct-publicize.lua"
     dependencies = {
         "luvit/coro-fs",
         "luvit/pathjoin",
-        "code-nuage/direct-mime"
+        "code-nuage/direct-mime",
+        "code-nuage/direct-colors"
     }
     description = "The publicize plugin of the direct web microframework."
     tags = { "direct" }
@@ -22,6 +23,7 @@
 local fs = require("coro-fs")
 local path_join = require("pathjoin").pathJoin
 local mime = require("direct-mime")
+local colors = require("direct-colors")
 
 local M = {}
 M._NAME = "direct-publicize"
@@ -32,29 +34,34 @@ function M.on_load(app, directory, path_prefix)
     path_prefix = path_prefix or ""
 
     return coroutine.wrap(function()
-        local entries, err = fs.scandir(directory)
-        if not entries then
-            return nil, err
-        end
+        for entry, scan_err in fs.scandir(directory) do
+            if not entry then
+                error(string.format("Can't scan: %s", scan_err))
+            end
 
-        for _, entry in ipairs(entries) do
-            local full_path = path_join(directory, entry)
-            local stat, stat_err = fs.stat(full_path)
-            if stat then
-                if stat.type == "file" then
-                    local route = path_prefix .. "/" .. entry
-                    app:add_route(route, "GET", function(req, res)
-                        local data, read_err = fs.readFile(full_path)
-                        res:set_code(200)
-                        res:set_header("Content-Type", mime.guess(entry))
-                        res:set_body(data or "")
-                    end)
-                elseif stat.type == "directory" then
-                    M.on_load(app, full_path, path_prefix .. "/" .. entry)()
-                end
+            local full_path = path_join(directory, entry.name)
+
+            if entry.type == "file" then
+                local route = path_prefix .. "/" .. entry.name
+                app:add_route(route, "GET", function(req, res)
+                    local data, read_err = fs.readFile(full_path)
+
+                    if not data then
+                        print(string.format(colors.colorize("Cannot read file %s: %s", {color = "RED", variant = "BOLD"})),
+                            entry.name, read_err)
+                        return
+                    end
+
+                    res:set_code(200)
+                    res:set_header("Content-Type", mime.guess(entry.name))
+                    res:set_body(data or "")
+                end)
+            elseif entry.type == "directory" then
+                M.on_load(app, full_path, path_prefix .. "/" .. entry.name)
             end
         end
-    end)
+    end)()
 end
 
 return M
+
