069f59e53cf1e2e934c59a1a193412d9fcb55aca
code-nuage <eloi.hariot@protonmail.com>
2025-11-28 15:33:00 +0100

    v0.0.6

--- file.lua
+++ file.lua
@@ -1,7 +1,17 @@
 --[[lit-meta
     name = "code-nuage/direct"
-    version = "0.0.5"
+    version = "0.0.6"
     homepage = "https://github.com/code-nuage/direct/blob/main/direct.lua"
+    dependencies = {
+        "code-nuage/direct-server",
+        "code-nuage/direct-router",
+        "code-nuage/direct-reasons",
+        "code-nuage/direct-mime",
+        "code-nuage/direct-colors",
+        "code-nuage/direct-verbosity",
+        "code-nuage/direct-publicize",
+        "code-nuage/direct-logs"
+    }
     description = "direct web microframework."
     tags = { "direct" }
     license = "MIT"
@@ -21,6 +31,7 @@ return {
     -- Utils
     reasons = require("direct-reasons"),
     mime = require("direct-mime"),
+    colors = require("direct-colors"),
 
     -- Router's plugins
     verbosity = require("direct-verbosity"),
