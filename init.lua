617f850e8560d7e5c86aa3071d82f0986f861c38
code-nuage <eloi.hariot@protonmail.com>
2025-12-15 16:42:33 +0100

    v0.0.1

---
+++ init.lua
@@ -0,0 +1,30 @@
+-- lustache: Lua mustache template parsing.
+-- Copyright 2013 Olivine Labs, LLC <projects@olivinelabs.com>
+-- MIT Licensed.
+--
+-- Adapted by code-nuage
+-- Under the MIT licence
+
+local string_gmatch = string.gmatch
+
+function string.split(str, sep)
+  local out = {}
+  for m in string_gmatch(str, "[^"..sep.."]+") do out[#out+1] = m end
+  return out
+end
+
+local lustache = {
+  name     = "lustache",
+  version  = "0.0.1",
+  renderer = require("./renderer.lua"):new(),
+}
+
+return setmetatable(lustache, {
+  __index = function(self, idx)
+    if self.renderer[idx] then return self.renderer[idx] end
+  end,
+  __newindex = function(self, idx, val)
+    if idx == "partials" then self.renderer.partials = val end
+    if idx == "tags" then self.renderer.tags = val end
+  end
+})
---
+++ package.lua
@@ -0,0 +1,15 @@
+return {
+    name = "code-nuage/lustache",
+    version = "0.0.1",
+    description = "A simple description of my little package.",
+    tags = { "lua", "lit", "luvit" },
+    license = "MIT",
+    author = { name = "code-nuage", email = "eloi.hariot@protonmail.com" },
+    homepage = "https://github.com/code-nuage/lustache",
+    dependencies = {},
+    files = {
+        "**.lua",
+        "!test*"
+    }
+}
+
