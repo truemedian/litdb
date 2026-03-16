b14e84dac8246d61e4ec4bb89ef61b0b32a3f412
Bilal2453 <belal2453@gmail.com>
2026-03-16 16:31:09 +0300

    v2.8.0-5

--- init.lua
+++ init.lua
@@ -1,12 +1,37 @@
 local module_name = 'luasql_odbc'
 
 local uv = require('uv')
-local os = uv.os_uname().sysname:lower()
-local pathjoin = require('luvipath').pathJoin
+local os = uv.os_uname().sysname:lower():match('%P+')
 
-local scan_hnd = uv.fs_scandir(module.dir)
-for name, kind in uv.fs_scandir_next, scan_hnd do
-  if name:lower():find(os) and kind == 'directory' then
-    return require(pathjoin(module.dir, name, module_name))
+local luvi = require('luvi')
+local is_bundle = module.path:match('^@?bundle:.*') and true or false
+local pathjoin = luvi.path.join
+local stat = is_bundle and luvi.bundle.stat or uv.fs_stat
+
+local dirs = {
+  'Windows-x64',
+  'Windows-x86_64',
+  'Linux-x64',
+  'Linux-x86_64',
+}
+
+-- a hacky fix for a luvit's require bug (see #1238)
+-- we reverse this change after being done with it
+local org_dir = module.dir
+local module_dir = module.dir:gsub('^@?bundle:', '')
+
+local mod
+for _, dir_name in ipairs(dirs) do
+  local dir_path = pathjoin(module_dir, dir_name)
+  if dir_name:lower():find(os) and stat(dir_path) then
+    module.dir = pathjoin(module.dir, dir_name)
+    mod = require('./' .. module_name)
+    if mod then
+      break
+    end
   end
 end
+
+module.dir = org_dir
+return mod or error('luasql shared library could not be found. \n\
+Try reinstalling the library otherwise a pre-built binary might not be available on this platform')
--- package.lua
+++ package.lua
@@ -1,6 +1,6 @@
 return {
   name = "Bilal2453/luasql-odbc",
-  version = "2.8.0-4",
+  version = "2.8.0-5",
   description = "LuaSQL is a simple interface from Lua to a DBMS. This build includes ODBC driver only.",
   tags = {},
   license = "MIT",
